a06619ac60ec91534deb2b10a2ad1f000085aab9
Li Zheng <lzszone@gmail.com>
2017-10-16 11:14:29 +0800

    add

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use(async (ctx, next) => {
   ctx.reqTime = new Date();
   ctx.db = db;
   ctx.nkcModules = nkcModules;
+  Object.defineProperty(ctx, 'template', {
+    template: {
+      get: () => './pages/' + this.__templateFile,
+      set: fileName => this.__templateFile = fileName
+    }
+  });
   try {
     await next();
     ctx.status = ctx.response.status
@@ -34,4 +40,16 @@ app.use(async (ctx, next) => {
 app.use(staticServe('./pages'));
 app.use(bodyParser());
 app.use(mainRouter.routes());
+app.use(async (ctx, next) => {
+  switch(ctx.type) {
+    case 'text/html':
+      ctx.body = ctx.nkcModules.render(ctx.template, {data: ctx.data});
+      break;
+    case 'application/json':
+      ctx.body = ctx.data;
+      break;
+  }
+  next();
+});
+
 module.exports = app.callback();
--- routes/experimental/index.js
+++ routes/experimental/index.js
@@ -4,7 +4,7 @@ const experimentalRouter = new Router();
 
 experimentalRouter
   .get('/', async (ctx, next) => {
-    ctx.body = `管`;
+    ctx.data = `管`;
     next();
   });
   // .use('/set', setRouter.routes(), setRouter.allowedMethods())
