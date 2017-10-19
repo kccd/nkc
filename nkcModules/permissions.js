834f84b6b104bab8b5f60ea5147c2ec7851637a4
Li Zheng <lzszone@gmail.com>
2017-10-19 15:30:38 +0800

    add.

--- nkcModules/permissions.js
+++ nkcModules/permissions.js
@@ -1,81 +1,135 @@
-const visitor = {
-  displayName: '陆游',
-  contentClasses: {
-    null: true,
-    images: true,
-    non_public: false,
-    non_images: false
-  },
-  permittedOperations: {
-    GET: true,
-    login: {
-      displayName: '登录',
+const parameter = Symbol('parameter');
+const certificates ={
+  visitor: {
+    displayName: '陆游',
+    contentClasses: {
+      null: true,
+      images: true,
+      non_public: false,
+      non_images: false
+    },
+    permittedOperations: {
       GET: true,
-      POST: true,
-      DELETE: true
-    },
-    latest: {
-      displayName: '最近',
-      GET: true
-    },
-    activities: {
-      displayName: '动态',
-      GET: true,
-      nextIsParam: true
-    },
-    editor: {
-      displayName: '编辑器',
-      GET: true
-    },
-    t: {
-      displayName: '贴子',
-      GET: true,
-      POST: true,
-      nextIsParam: true,
-      digest: {
-        displayName: '精华',
-        GET: true,
-        DELETE: true,
-      }
-    },
-    f: {
-      displayName: '板块',
-      GET: true,
-      nextIsParam: true
-    },
-    u: {
-      displayName: '用户',
-      GET: true,
-      nextIsParam: true,
-      favorite: {
-        displayName: '收藏',
+      login: {
+        displayName: '登录',
         GET: true,
         POST: true,
         DELETE: true
+      },
+      latest: {
+        displayName: '最近',
+        GET: true
+      },
+      activities: {
+        displayName: '动态',
+        [parameter]: {
+          GET: true
+        }
+      },
+      editor: {
+        displayName: '编辑器',
+        GET: true
+      },
+      t: {
+        displayName: '贴子',
+        [parameter]: {
+          GET: true
+        }
+      },
+      f: {
+        displayName: '板块',
+        GET: true,
+        [parameter]: {
+          GET: true
+        }
+      },
+      u: {
+        displayName: '用户',
+        [parameter]: {
+          GET: true
+        }
+      },
+      m: {
+        displayName: '专栏',
+        [parameter]: {
+          GET: true
+        }
+      },
+      search: {
+        displayName: '搜索',
+        GET: true,
+        POST: true,
+      },
+      exam: {
+        displayName: '考试',
+        [parameter]: {
+          GET: true,
+          POST: true,
+        }
+      },
+      register: {
+        displayName: '注册',
+        [parameter]: {
+          GET: true,
+          POST: true,
+        }
+      },
+      r: {
+        displayName: '附件',
+        [parameter]: {
+          GET: true,
+          nextIsParam: true}
+        },
+      rt: {
+        displayName: '附件',
+        [parameter]: {
+          GET: true
+        }
+      },
+      p: {
+        displayName: '推文',
+        [parameter]: {
+          GET: true,
+        }
       }
-    },
-    m: {
-      displayName: '专栏',
-      GET: true,
-      nextIsParam: true
-    },
-    search: {
-      displayName: '搜索',
-      GET: true,
-      POST: true,
-    },
-    exam: {
-      displayName: '考试',
-      GET: true,
-      POST: true,
-      nextIsParam: true,
-    },
-    register: {
-      displayName: '注册',
-      GET: true,
-      POST: true,
-      nextIsParam: true
     }
+  },
 
+  default: {
+    displayName: '会员',
+    inheritFrom: ['visitor'],
+    contentClasses: {
+      images: true,
+      non_public: true,
+      non_images: true,
+      non_broadcast: true
+    },
+    permittedOperations: {
+      f: {
+        [parameter]: {
+          POST: true,
+        }
+      },
+      sms: {
+        displayName: '信息',
+        GET: true,
+        POST: true,
+        nextIsParam: true,
+        at: {
+          GET: true
+        },
+        replies: {
+          GET: true
+        },
+        message: {
+          GET: true,
+          nextIsParam: true,
+          POST: true
+        },
+        system: {
+          GET: true
+        }
+      },
+    }
   }
 };
