4b90face725cb27eab1ca9a4623289e23de23cfb
pengxigui <p_xigui@163.com>
2017-10-27 16:25:48 +0800

    add

--- dataModels/SettingModel.js
+++ dataModels/SettingModel.js
@@ -43,7 +43,14 @@ async function operateSystemID(type, op) {
 
 settingSchema.statics.operateSystemID = operateSystemID;
 let Setting = mongoose.model('settings', settingSchema);
-new Setting({uid: 'system',ads: [1], popPersonalForums:[1],counters:{
+new Setting({uid: 'system',ads: [
+  '82312',
+  '82340',
+  '82333',
+  '82282',
+  '80787',
+  '65452'
+], popPersonalForums:['10','74185','73327','74156','74856','78451'],counters:{
   resources: 1,
   users: 80000,
   posts: 840234,
