d4a5bc471084f15cf92655b8baf5622e76c2f620
Li Zheng <lzszone@gmail.com>
2017-10-27 11:20:35 +0800

    add

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(staticServe('./nkcModules'));
 app.use(favicon(__dirname + '/resources/site_specific/favicon.ico'));
 // app.use(permissions);
 app.use(mainRouter.routes());
-app.use(body);aasdasddasdasdsaddfdsfasdasdasd
+app.use(body);aasdasddasdasdsaddfdsfasdasdasdsdfsdf
 /asdfasdfdsafasdfdsaf/
 module.exports = app.callback();
