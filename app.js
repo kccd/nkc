473a387dc5464ab9478237b7918d9ef2ee375583
Li Zheng <lzszone@gmail.com>
2017-11-10 16:23:39 +0800

    add

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use(async (ctx, next) => {
   await next()
 });
 app.use(cookieIdentify);
-app.use(staticServe('./pages'));
 app.use(staticServe('./resources'));
+app.use(staticServe('./pages'));
 app.use(staticServe('./node_modules'));
 app.use(staticServe('./nkcModules'));
 app.use(favicon(__dirname + '/resources/site_specific/favicon.ico'));
