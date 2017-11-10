b6f7de331c6054fcc7d506e9bfb756aa4abe004b
Li Zheng <lzszone@gmail.com>
2017-11-10 18:01:22 +0800

    add

--- middlewares/body.js
+++ middlewares/body.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 module.exports = async (ctx, next) => {
   if(ctx.filePath) {
     console.log(ctx.type);
-    ctx.type = 'image';
+    ctx.type = 'image/jpg';
     console.log(ctx.type);
     ctx.body = fs.createReadStream(ctx.filePath);
     await next();
