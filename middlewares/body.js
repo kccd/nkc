4d609b9ad63af8d54741c1fa4bb605624ff67b25
Li Zheng <lzszone@gmail.com>
2017-11-13 14:47:54 +0800

    add

--- middlewares/body.js
+++ middlewares/body.js
@@ -2,34 +2,24 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = async (ctx, next) => {
-  // if(ctx.filePath) {
-  //   console.log(ctx.type);
-  //   ctx.type = 'image/jpg';
-  //   console.log(ctx.response.type);
-  //   console.log(ctx.type);
-  //   console.log(JSON.stringify(ctx.is('image')));
-  //   ctx.body = fs.createReadStream(ctx.filePath);
-  //   await next();
-  // } else {
-  console.log(ctx.request.get('Accept'));
-    const type = ctx.request.accepts('json', 'html', 'image/*');
-    console.log(type);
-    switch(type) {
-      case 'json':
-        ctx.type = 'json';
-        ctx.body = ctx.data;
-        break;
-      case 'html':
-        ctx.type = 'html';
-        ctx.body = ctx.nkcModules.render(ctx.template, ctx.data);
-        break;
-      case 'image/*':
-        ctx.type = 'image';
-        ctx.body = fs.createReadStream(ctx.filePath);
-        break;
-      default:
-        ctx.throw(406, 'type not accectable')
-    }
-    await next();
-  // }
+  const type = ctx.request.accepts('json', 'html', 'image/*');
+  console.log(ctx.type);
+  console.log(type);
+  switch(type) {
+    case 'json':
+      ctx.type = 'json';
+      ctx.body = ctx.data;
+      break;
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = ctx.nkcModules.render(ctx.template, ctx.data);
+      break;
+    case 'image/*':
+      ctx.type = 'image';
+      ctx.body = fs.createReadStream(ctx.filePath);
+      break;
+    default:
+      ctx.throw(406, 'type not accectable')
+  }
+  await next();
 };
