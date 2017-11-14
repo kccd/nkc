16c96a71c4695eb8964316a3e106fd8b191114d5
pengxigui <p_xigui@163.com>
2017-11-14 16:58:40 +0800

    add

--- settings/statics.js
+++ settings/statics.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const watermark = path.resolve('resources/default_things/default_watermark3.png');
-const banner = path.resolve('site_specific/ad_default.jpg');
+const banner = path.resolve('resources/site_specific/ad_default.jpg');
 module.exports = {
   watermark,
   banner
