cfcf4c84a6d351af92b93e3ed19ef35c1cd5fa95
Li Zheng <lzszone@gmail.com>
2017-11-14 17:28:24 +0800

    add

--- dataModels/PostModel.js
+++ dataModels/PostModel.js
@@ -95,6 +95,7 @@ postSchema.pre('save' , function(next) {
 
 postSchema.methods.extendUser = async function() {
   const user = await UserModel.findOnly({uid:this.uid});
+  console.log(user);
   this.set({user});
   console.log(this.toObject())
 };
