fe7708c65abf205c483e12bfd9d5161662da6216
pengxigui <p_xigui@163.com>
2018-01-10 19:07:52 +0800

    学者

--- middlewares/cookieIdentify.js
+++ middlewares/cookieIdentify.js
@@ -15,7 +15,18 @@ module.exports = async (ctx, next) => {
       return ctx.redirect('/login')
     }
     await user.update({tlv: Date.now()});
-    if(user.xsf > 0 && !user.certs.includes('qc')) user.certs.push('qc');
+    if(user.xsf > 0){
+    	if (!user.certs.includes('scholar')) {
+		    user.certs.push('scholar');
+	    }
+    } else {
+			if(user.certs.includes('scholar')) {
+				const certs = user.certs;
+				const index = certs.indexOf('scholar');
+				certs.splice(index, 1);
+				await user.update({certs: certs});
+			}
+    }
     if(user.certs.includes('banned')) user.certs = ['banned'];
     user.newMessage = (await db.UsersPersonalModel.findOne({uid})).newMessage;
     user.subscribeUsers = (await db.UsersSubscribeModel.findOne({uid})).subscribeUsers;
