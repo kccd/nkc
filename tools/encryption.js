d98309c01ffa3aff5d5d2662ed6b73f5f810c97a
pengxigui <p_xigui@163.com>
2018-01-10 10:01:54 +0800

    add

--- tools/encryption.js
+++ tools/encryption.js
@@ -2,11 +2,17 @@ const crypto = require('crypto');
 
 module.exports = {
   encryptInMD5WithSalt: (password, salt) => {
-    const md5 = crypto.createHash('md5');
+  	const md5 = (str) => {
+			const md5 = crypto.createHash('md5');
+			md5.update(str);
+			return md5.digest('hex');
+	  };
+  	return md5(md5(password)+salt);
+    /*const md5 = crypto.createHash('md5');
     md5.update(password);
     const hashed = md5.digest('hex') + salt;
     md5.update(hashed);
-    return md5.digest('hex')
+    return md5.digest('hex')*/
   },
   encryptInSHA256HMACWithSalt: (password, salt) => {
     const hmac = crypto.createHmac('sha256', salt);
