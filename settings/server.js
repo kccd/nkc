f104d79cdd205546e1ccaf05d5336aed92e6fbbe
pengxigui <p_xigui@163.com>
2018-02-08 21:57:04 +0800

    add

--- settings/server.js
+++ settings/server.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: `nkc ${process.env.NODE_ENV || 'Development'} Server`,
-  copyright: '(c)2016 Guangdong Aililun(LoveTheory) Technology Co Ltd',
+  copyright: '(c)2018 kechuang institute',
   port: 9000,
   httpsPort: 10443,
-  github: 'https://github.com/lovetheory/nkc2',
+  github: 'https://github.com/kccd/nkc',
   useHttps: false,
   ACMEPath: 'F:/KC2016/.well-known',
   DBName: 'rescue'
