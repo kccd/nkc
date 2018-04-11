74e23c43726962c8402e1fb35cc69e404ffc1d23
pengxigui <p_xigui@163.com>
2018-04-11 18:43:33 +0800

    前端，关注，搜索前端

--- pages/interface_fund_agreement.js
+++ pages/interface_fund_agreement.js
@@ -2,7 +2,7 @@ var fnString = $('#submit').attr('onclick');
 $('#submit').attr('onclick', '');
 $('input[type="radio"]').on('click', function() {
 	var arr = $('input[type="radio"]');
-	if(arr.eq(0).is(':checked') && arr.eq(2).is(':checked')) {
+	if(arr.eq(0).is(':checked')) {
 		$('#submit').removeClass('disabled').addClass('btn-info').attr('onclick', fnString);
 	} else {
 		$('#submit').addClass('disabled').removeClass('btn-info').attr('onclick', '');
