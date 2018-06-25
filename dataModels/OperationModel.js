83de1f6acf9a16ad297bdb6fa128d11017e3b692
pengxigui <p_xigui@163.com>
2018-06-25 11:00:00 +0800

    add

--- dataModels/OperationModel.js
+++ dataModels/OperationModel.js
@@ -23,29 +23,6 @@ const operationSchema = new Schema({
 		type: [Number],
 		index: 1,
 		default: []
-	},
-	kcb: {
-		status: {
-			type: Boolean,
-			default: false,
-			index: 1
-		},
-		count: {
-			type: Number,
-			default: 0
-		},
-		targetCount: {
-			type: Number,
-			default: 0
-		},
-		change: {
-			type: Number,
-			default: 0
-		},
-		targetChange: {
-			type: Number,
-			default: 0
-		}
 	}
 },{
 	collection: 'operations'
