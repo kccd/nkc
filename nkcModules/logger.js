73ee7cee089151f103bbd2c7728c8f867ddb3823
pengxigui <p_xigui@163.com>
2019-02-12 11:02:09 +0800

    显示ip

--- nkcModules/logger.js
+++ nkcModules/logger.js
@@ -30,6 +30,7 @@ module.exports = async (ctx) => {
   };
   if(ctx.logIt) {
     d.from = ctx.req.headers.referer;
+    d.address = ctx.address;
     if (ctx.error) {
       console.error(
         `${moment().format('YYYY/MM/DD HH:mm:ss').grey} ${(' ' + global.NKC.processId + ' ').grey} ${' Error '.bgRed} ${log.uid.bgCyan} ${log.method.black.bgYellow} ${log.path.bgBlue} <${processTime.green}ms> ${String(log.status).red} ${(operation?operation.description: '未知操作').grey}`
