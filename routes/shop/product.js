cf176fc788ae96fa546555072ba27a43afd58a55
kris <1015650506@qq.com>
2019-02-22 14:57:12 +0800

    add

--- routes/shop/product.js
+++ routes/shop/product.js
@@ -9,6 +9,15 @@ productRouter
     if(!product) ctx.throw(404, "商品不存在");
     // 取出商品全部评论
     // 如果商品存在
+    let c;
+    if(product){
+      c = product.productDetails;
+    }
+    const post = {
+      c:c,
+      l:'html'
+    }
+    product.productDetails = ctx.nkcModules.nkc_render.experimental_render(post);
     data.productInfo = product;
     ctx.template = "shop/product/index.pug";
     await next();
