c3771b928402747d91af43c2c1373abb85ea8266
pengxigui <p_xigui@163.com>
2019-03-12 14:22:40 +0800

    add

--- pages/account/finance/recharge.js
+++ pages/account/finance/recharge.js
@@ -1,13 +1,35 @@
 var app = new Vue({
   el: '#app',
   data: {
-    defaultKCB: [10, 20, 50, 100, 1000],
-    money: '',
+    defaultKCB: [10, 20, 50, 100, 500, 1000, 5000],
+    money: 10,
+    error: '',
+    payment: 'alipay',
     input: ''
   },
   methods: {
     select: function(m) {
       this.money = m;
+    },
+    pay: function() {
+      this.error = '';
+      var money = 0;
+      if(this.money) {
+        money = this.money;
+      } else {
+        money = this.input;
+      }
+      if(money > 0){}
+        else {
+          return this.error = '充值数额必须大于0';
+        }
+      kcAPI('/account/finance/recharge?type=get_url&money=' + money, 'GET')
+        .then(function(data) {
+          alert(data.url);
+        })
+        .catch(function(data) {
+          this.error = data.error || data;
+        });
     }
   }
 });
--- routes/account/finance/recharge.js
+++ routes/account/finance/recharge.js
@@ -2,6 +2,21 @@ const Router = require('koa-router');
 const router = new Router();
 router
   .get('/', async (ctx, next) => {
+    const {query, data, nkcModules} = ctx;
+    const {user} = data;
+    let {type, money} = query;
+    if(type === 'get_url') {
+      money = Number(money);
+      if(money > 0){}
+      else {
+        ctx.throw(400, '充值数额必须大于0');
+      }
+      const options = {
+        money,
+        id: 
+      };
+      data.url = await nkcModules.alipay2.receipt(options);
+    }
     ctx.template = 'account/finance/recharge.pug';
     await next();
   });
