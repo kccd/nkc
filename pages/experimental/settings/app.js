666f650b127666a3fdc796de83aaa4cb777f10fb
kris <1015650506@qq.com>
2019-07-16 19:55:35 +0800

    add

--- pages/experimental/settings/app.js
+++ pages/experimental/settings/app.js
@@ -1,12 +1,22 @@
 function submitApp() {
+  geid('submitApp').disabled = true;
   var platform = $('#platform').val();
   var version = $('#version').val();
   var description = $('#description').val();
   var toc = new Date().getDate();
-  if(!version) return screenTopAlert("请输入版本号！");
-  if(!description) return screenTopAlert("请输入更新内容！");
+  if(!version) {
+    geid('submitApp').disabled = false;
+    return screenTopAlert("请输入版本号！");
+  }
+  if(!description) {
+    geid('submitApp').disabled = false;
+    return screenTopAlert("请输入更新内容！");
+  }
   var file = geid('appfile').files;
-  if(file.length == 0) return screenTopAlert("请选择一个安装包！")
+  if(file.length == 0) {
+    geid("submitApp").disabled = false;
+    return screenTopAlert("请选择一个安装包！");
+  }
   var formData = new FormData();
   formData.append("file", file[0])
   formData.append("appPlatform",platform);
@@ -15,9 +25,11 @@ function submitApp() {
   formData.append("appToc", toc);
   uploadFilePromise("/e/settings/app/upload", formData)
   .then(function(data) {
-    screenTopAlert("上传成功")
+    screenTopAlert("上传成功");
+    geid('submitApp').disabled = false;
   })
   .catch(function(data) {
-    screenTopAlert(data.error || data)
+    screenTopAlert(data.error || data);
+    geid('submitApp').disabled = false;
   })
 }
