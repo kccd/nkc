cc40a9f6f73db4c19dcb70f4e0043aa02a23c306
pengxigui <p_xigui@163.com>
2019-08-15 16:30:15 +0800

    add

--- routes/account/index.js
+++ routes/account/index.js
@@ -3,6 +3,10 @@ const router = new Router();
 const financeRouter = require('./finance');
 const contributeRouter = require("./contribute");
 router
+  .get("/", async (ctx, next) => {
+    ctx.template = "account/account.pug";
+    await next();
+  })
   .use("/contribute", contributeRouter.routes(), contributeRouter.allowedMethods())
   .use('/finance', financeRouter.routes(), financeRouter.allowedMethods());
 module.exports = router;
