eac2a49b5660d2e20dcd60c7648894f2aaaecf30
pengxigui <p_xigui@163.com>
2019-08-15 10:52:14 +0800

    优化查看匿名内容作者的方式

---
+++ pages/publicModules/floatUserPanel/userInfo.js
@@ -0,0 +1,43 @@
+NKC.modules.UserInfo = function() {
+  var self = this;
+  self.dom = $("#moduleUserInfo");
+  self.dom.modal({
+    show: false
+  });
+  self.app = new Vue({
+    el: "#moduleUserInfoApp",
+    data: {
+      user: "",
+      loading: true
+    }
+  });
+  self.open = function(options) {
+    // type: showUserByUid, showUserByPid
+    options = options || {
+      type: "showUserByUid"
+    };
+    self.app.loading = true;
+    var type = options.type;
+    if(type === "showUserByPid") {
+      nkcAPI("/p/" + options.pid + "/author", "GET")
+        .then(function(data) {
+          self.app.user = data.author;
+          self.app.loading = false;
+        })
+        .catch(function(data) {
+          sweetError(data);
+        })
+
+    } else if(type === "showUserByUid") {
+      nkcAPI("/u/" + options.uid + "?from=panel", "GET")
+        .then(function (data) {
+          self.app.user = data.targetUser;
+          self.app.loading = false;
+        })
+        .catch(function (data) {
+          sweetError(data);
+        })
+    }
+    self.dom.modal("show");
+  }
+}
