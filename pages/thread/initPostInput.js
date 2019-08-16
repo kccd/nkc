7149c6454dd2485531eab82ad5709334df14861c
pengxigui <p_xigui@163.com>
2019-08-16 14:34:50 +0800

    修复回复框全屏会被header挡住的问题

--- pages/thread/initPostInput.js
+++ pages/thread/initPostInput.js
@@ -14,10 +14,10 @@ var ue = UE.getEditor('container', {
     ]
   ],
   maximumWords: 100000, // 最大字符数
-  initialFrameHeight: initialFrameHeight, // 编辑器高度
+  initialFrameHeight: 200, // 编辑器高度
   autoHeightEnabled:false, // 编辑器是否随着行数增加而自动长高
   scaleEnabled: true, // 是否允许拉长
-  topOffset: topOffset, // toolbar工具栏在滚动时的固定位置
+  topOffset: 47, // toolbar工具栏在滚动时的固定位置
   //- imagePopup: false, // 是否开启图片调整浮层
   //- enableContextMenu: false, // 是否开启右键菜单
   enableAutoSave: false, // 是否启动自动保存
