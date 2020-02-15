24d44291dda89507cf4530584568418654f6c1ad
pengxigua <pengxiguaa@gmail.com>
2020-02-15 11:48:02 +0800

    修复交易地址页面报错问题

--- routes/user/transaction.js
+++ routes/user/transaction.js
@@ -10,7 +10,6 @@ router
 		const targetUserPersonal = await db.UsersPersonalModel.findOnly({uid});
 		data.addresses = targetUserPersonal.addresses;
 		data.personalInfo = targetUserPersonal.personalInfo;
-		data.targetUserSubscribe = await db.UsersSubscribeModel.findOnly({uid});
 		ctx.template = 'user/transaction.pug';
 		await next();
 	});
