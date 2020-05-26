0c6d963849a930043dbe6a7beced75f0b325d70c
pengxigui <p_xigui@163.com>
2020-05-26 13:50:56 +0800

    add

--- pages/publicModules/common.js
+++ pages/publicModules/common.js
@@ -717,7 +717,7 @@ NKC.methods.toChat = function(uid, name, type) {
   if(NKC.configs.platform === 'reactNative') {
     NKC.methods.rn.emit('toChat', {
       uid: uid,
-      name: name || uid,
+      name: name,
       type: type || 'UTU'
     });
   } else {
