f0e7f80b64d8721467e713d324920fc28f9fc586
pengxigui <p_xigui@163.com>
2020-07-23 17:36:41 +0800

    add

--- routes/fund/donation.js
+++ routes/fund/donation.js
@@ -78,61 +78,6 @@ donationRouter
 		}
 		await next();
 	})
-	.post('/', async (ctx, next) => {
-		const {data, db, body} = ctx;
-		const {fundId, anonymous, type} = body;
-		const money = body.money?parseInt(body.money): 0;
-		let abstract;
-		if(type === 'refund') {
-			if(money <= 0) {
-				ctx.throw(400, '退款金额错误！');
-			}
-			abstract = '退款';
-		} else {
-			abstract = '赞助';
-			if(money < 20) {
-				ctx.throw(400, '单笔赞助金额不能少于20元。');
-			}
-			if(money > 10000) {
-				ctx.throw(400, '单笔赞助金额不能超过10000元，请分批次赞助。');
-			}
-		}
-		const {user} = data;
-		if(!anonymous && !user) ctx.throw(400, '非匿名赞助要求用户必须登录，请登录后再试。');
-		const id = Date.now();
-		const bill = await db.FundBillModel.findOne({_id: id});
-		if(bill) ctx.throw(500, '交易编号重复，请重新提交。');
-		let fund;
-		if(fundId) {
-			fund = await db.FundModel.findOnly({_id: fundId});
-		}
-		const params = {
-			out_trade_no: id,
-			subject: `科创基金${fund?` - ${fund.name}`: ' - 资金池'}`,
-			body: `${abstract}${money}元`,
-			total_fee: money
-		};
-		data.url = directAlipay.buildDirectPayURL(params);
-		const newBill = db.FundBillModel({
-			_id: id,
-			uid: user?user.uid: '',
-			abstract: abstract,
-			notes: `${abstract}${money}元`,
-			money: money,
-			from: {
-				type: 'user',
-				id: user?user.uid: '',
-				anonymous: !!anonymous
-			},
-			to: {
-				type: fundId?'fund': 'fundPool',
-				id: fundId?fundId: ''
-			},
-			verify: false
-		});
-		await newBill.save();
-		await next();
-	})
 	.get('/return', async (ctx, next) => {
 		const {data, query, db} = ctx;
 		await directAlipay.verify(query);
