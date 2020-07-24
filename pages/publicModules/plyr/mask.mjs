8ee3d10577dcb911d475c33e1efed520b345d8bf
yuu <tokisakiyuu@qq.com>
2020-07-24 18:22:57 +0800

    消息文件存储位置调整

--- pages/publicModules/plyr/mask.mjs
+++ pages/publicModules/plyr/mask.mjs
@@ -4,6 +4,7 @@ if(!threadSettings.isDisplay) {
 } else {
   NKC.methods.initPlyrMask = function(player) {
     const container = player.elements.container;
+    $(container).find(".plyr__control.plyr__control--overlaid").remove();
     let maskDom = $("#plyrMask .plyr-mask").clone(false);
     let maskPlayButton = maskDom.find(".player-tip-button .play");
     let maskDownloadButton = maskDom.find(".player-tip-button .download");
