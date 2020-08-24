82155cdcd14447bddae943ea4897cdc83927a477
pengxigui <p_xigui@163.com>
2020-08-24 11:39:43 +0800

    后台管理基本设置中添加网站代号

--- routes/experimental/settings/base/index.js
+++ routes/experimental/settings/base/index.js
@@ -9,13 +9,15 @@ baseRouter
 	})
 	.put('/', async (ctx, next) => {
 		const {db, body} = ctx;
-		let {links, websiteName, websiteAbbr, github, backgroundColor, copyright, record, description, keywords, brief, telephone, statement} = body;
+		let {links,websiteCode ,websiteName, websiteAbbr, github, backgroundColor, copyright, record, description, keywords, brief, telephone, statement} = body;
 		if(!websiteName) ctx.throw(400, '网站名不能为空');
+		if(!websiteCode) ctx.throw(400, `网站代号不能为空`);
 		websiteName = websiteName.trim();
 		const serverSettings = await db.SettingModel.findOnly({_id: 'server'});
 		const keywordsArr = keywords.split(',');
 		const obj = {
 		  c: {
+				websiteCode,
 				websiteName,
 				websiteAbbr,
         github,
