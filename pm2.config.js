be86814afa7a902bb25c8aacbd0f3ea879212833
pengxigui <p_xigui@163.com>
2021-01-28 15:57:54 +0800

    add

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,55 @@
 module.exports = {
   apps: [
     {
-      name: 'kc',
+      name: 'nkc',
       script: 'server.js',
       shutdown_with_message: true,
-      restart_delay: 10000, // 崩溃后重启前的等待毫秒数
+      wait_ready: true,
+      instances: 12,
+      exec_mode: 'cluster',
+      restart_delay: 5000, // 崩溃后重启前的等待毫秒数
+      increment_var: 'PROCESS_ID',
       env: {
         NODE_ENV: 'production',
-        PROCESS_ID: 0
+        PROCESS_ID: 0,
+      }
+    },
+    {
+      name: 'socket',
+      script: 'socketServer.js',
+      wait_ready: true,
+      shutdown_with_message: true,
+      instances: 8,
+      exec_mode: 'cluster',
+      restart_delay: 5000, // 崩溃后重启前的等待毫秒数
+      increment_var: 'PROCESS_ID',
+      env: {
+        NODE_ENV: 'production',
+        PROCESS_ID: 0,
+      }
+    },
+    {
+      name: 'timed task',
+      script: 'timedTask.js',
+      wait_ready: true,
+      shutdown_with_message: true,
+      restart_delay: 5000, // 崩溃后重启前的等待毫秒数
+      increment_var: 'PROCESS_ID',
+      env: {
+        NODE_ENV: 'production',
+        PROCESS_ID: 0,
+      }
+    },
+    {
+      name: 'communication',
+      script: 'communicationServer.js',
+      wait_ready: true,
+      shutdown_with_message: true,
+      restart_delay: 5000, // 崩溃后重启前的等待毫秒数
+      increment_var: 'PROCESS_ID',
+      env: {
+        NODE_ENV: 'production',
+        PROCESS_ID: 0,
       }
     }
   ]
