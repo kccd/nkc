384aa6134bd4ca146301e90b14671a718ea6b1e8
tokisakiyuu <tokisakiyuu@qq.com>
2021-03-02 11:54:46 +0800

    feat: 考试系统主页考试须知编辑

--- pages/experimental/settings/exam.js
+++ pages/experimental/settings/exam.js
@@ -1,11 +1,3 @@
-var editor = new wangEditor('#editor');
-editor.config.zIndex = 1;
-editor.config.excludeMenus = ["video", "code", "splitLine"];
-editor.config.placeholder = "请在此编辑考试须知";
-editor.config.onchange = function(newHtml) {
-  app.examSettings.examNotes = newHtml;
-}
-
 var app = new Vue({
   el: '#app',
   data: {
@@ -15,14 +7,12 @@ var app = new Vue({
     var data = document.getElementById('data');
     data = JSON.parse(data.innerHTML);
     var settings = data.examSettings;
-    console.log(settings);
     var examSettings = {};
     examSettings.count = settings.count || 0;
     examSettings.countOneDay = settings.countOneDay || 0;
     examSettings.waitingTime = settings.waitingTime || 0;
     examSettings.examNotes = settings.examNotes || "";
     this.examSettings = examSettings;
-    editor.create();
   },
   methods: {
     save: function() {
