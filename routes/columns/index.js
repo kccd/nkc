12934df1c75376a55cad3a80d9d068a11ed4034d
pengxigui <p_xigui@163.com>
2021-03-09 16:25:09 +0800

    feat: 添加专栏列表页

--- routes/columns/index.js
+++ routes/columns/index.js
@@ -4,6 +4,21 @@ const columnRouter = require("./column");
 
 router
   .get("/", async (ctx, next) => {
+    const {query, data, db} = ctx;
+    const {page = 0} = query;
+    const match = {};
+    if(!ctx.permission('column_single_disabled')) {
+      match.closed = false;
+      match.disabled = false;
+    }
+    const count = await db.ColumnModel.count(match);
+    const paging = ctx.nkcModules.apiFunction.paging(page, count);
+    data.columns = await db.ColumnModel.find(match)
+      .sort({toc: -1})
+      .skip(paging.start)
+      .limit(paging.perpage);
+    ctx.template = 'columns/columns.pug';
+    data.paging = paging;
     await next();
   })
   .use("/:_id", columnRouter.routes(), columnRouter.allowedMethods());
