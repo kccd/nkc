956ca86465f66f7f4fcf7ce7bea2319e4d77f316
pengxigui <p_xigui@163.com>
2021-03-09 11:47:13 +0800

    feat: 集成微信支付

---
+++ nkcModules/wechatPay/H5.js
@@ -0,0 +1,59 @@
+const wechatPayConfigs = require('../../config/wechatPay.json');
+const {getHeaderAuthInfo} = require('./util');
+const axios = require('axios');
+const func = {};
+
+/*
+* 获取h5支付链接，访问链接可唤起手机微信完成支付
+* 待验证，等审核通过后再完善
+* */
+func.getH5PaymentUrl = async (props) => {
+  const {description, orderId, money, clientIp} = props;
+  const timeExpire = new Date(Date.now() + wechatPayConfigs.orderTimeout);
+  const data = {
+    appid: wechatPayConfigs.appId,
+    mchid: wechatPayConfigs.mchId,
+    time_expire: timeExpire.toISOString(),
+    description,
+    out_trade_no: orderId,
+    attach: JSON.stringify({orderId}),
+    notify_url: wechatPayConfigs.notifyUrl,
+    amount: {
+      currency: 'CNY',
+      total: money
+    },
+    scene_info: {
+      payer_client_ip: '127.0.0.1',
+      h5_info: {
+        type: 'wap' // ios, android, wap
+      }
+    }
+  };
+  const authorization = await getHeaderAuthInfo(wechatPayConfigs.H5Url, 'POST', data);
+  return axios({
+    method: 'POST',
+    url: wechatPayConfigs.nativeUrl,
+    data,
+    headers: {
+      'Authorization': authorization
+    }
+  })
+    .then(response => {
+      return response.data.h5_url;
+    })
+    .catch(error => {
+      let status = 500, code = 'error', message = error.message || '';
+      if(error.response) {
+        status = error.response.status;
+        if(error.response.data) {
+          code = error.response.data.code;
+          message = error.response.data.message;
+        }
+      }
+      const err = new Error(`${status} ${code} ${message}`);
+      err.status = status;
+      throw err;
+    });
+};
+
+module.exports = func;
---
+++ nkcModules/wechatPay/util.js
@@ -0,0 +1,77 @@
+const wechatPayConfigs = require('../../config/wechatPay.json');
+const {getRandomString} = require('../apiFunction');
+const fs = require('fs');
+const crypto = require('crypto');
+const privateKey = fs.readFileSync(wechatPayConfigs.certFilePath).toString();
+
+const func = {};
+
+/*
+* 解密来自微信的请求
+* @param {Object} body 请求body
+* @return {String} 解密结果
+* */
+func.getNotificationInfo = async (body) => {
+  const {
+    id,
+    create_time,
+    resource_type,
+    event_type,
+    summary,
+    resource,
+  } = body;
+  const {
+    ciphertext,
+    associated_data: associatedData,
+    nonce,
+  } = resource;
+
+  const ciphertextBuffer = Buffer.from(ciphertext, 'base64');
+  const authTag = ciphertextBuffer.slice(ciphertextBuffer.length - 16);
+  const data = ciphertextBuffer.slice(0, ciphertextBuffer.length - 16);
+  const decipherIv = crypto.createDecipheriv('aes-256-gcm', wechatPayConfigs.APIv3, nonce);
+  decipherIv.setAuthTag(Buffer.from(authTag));
+  decipherIv.setAAD(Buffer.from(associatedData));
+  const decryptStr = decipherIv.update(data, null, 'utf-8');
+  decipherIv.final();
+  return decryptStr;
+};
+
+/*
+* 获取验证信息
+* @param {String} url
+* @param {String} method
+* @param {Object} data 请求body
+* @return {String}
+* */
+func.getHeaderAuthInfo = async (url, method, data) => {
+  method = method.toUpperCase();
+  let absoluteUrl = new URL(url);
+  absoluteUrl = absoluteUrl.pathname + absoluteUrl.search;
+  const nowTime = Math.floor(Date.now() / 1000);
+  const randomString = getRandomString(`A0`, 32);
+  let bodyString;
+  if (method === 'GET') {
+    bodyString = '\n';
+  } else {
+    bodyString = JSON.stringify(data) + '\n';
+  }
+  const content =
+    method + '\n' +
+    absoluteUrl + '\n' +
+    nowTime + '\n' +
+    randomString + '\n' +
+    bodyString;
+
+  const sign = crypto.createSign('RSA-SHA256');
+  sign.update(Buffer.from(content, 'utf-8'));
+  const signBase64 = sign.sign(privateKey, 'base64');
+
+  return (`WECHATPAY2-SHA256-RSA2048 mchid="${wechatPayConfigs.mchId}"` +
+    `,nonce_str="${randomString}"` +
+    `,signature="${signBase64}"` +
+    `,timestamp="${nowTime}"` +
+    `,serial_no="${wechatPayConfigs.certNumber}"`);
+};
+
+module.exports = func;
