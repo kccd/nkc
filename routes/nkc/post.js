52f30ed41cd0594221ddbf9e728a917b54aff466
pengxiguaa <pengxiguaa@gmail.com>
2021-04-27 16:01:37 +0800

    chore:优化前台管理最新发表页

--- routes/nkc/post.js
+++ routes/nkc/post.js
@@ -3,11 +3,37 @@ router
   .get("/", async(ctx, next) => {
     // 将最新的post直接渲染到页面上
     const {data, db, query, nkcModules} = ctx;
-    const {page = 0} = query;
-    const count = await db.PostModel.countDocuments();
-    const paging = nkcModules.apiFunction.paging(page, count, 50);
-    let posts = await db.PostModel.find({})
-      .sort({toc: -1}).skip(paging.start).limit(paging.perpage);
+    let {page = 0, t, c} = query;
+    if(c === undefined) {
+      c = Date.now();
+      let url = `/nkc/post?c=${c}`;
+      if(t) {
+        url += `&t=${t}`;
+      }
+      return ctx.redirect(url);
+    } else {
+      c = parseInt(c);
+    }
+    const match = {
+      toc: {$lte: c}
+    };
+    if(t === 'thread') {
+      match.type = 'thread';
+    } else if(t === 'post') {
+      match.type = 'post';
+    } else if(t === 'comment') {
+      match.type = 'post';
+      match.parentPostId = {$ne: ''};
+    } else {
+
+    }
+    const count = await db.PostModel.countDocuments(match);
+    const paging = nkcModules.apiFunction.paging(page, count, 100);
+    let posts = await db.PostModel
+      .find(match)
+      .sort({toc: -1})
+      .skip(paging.start)
+      .limit(paging.perpage);
     posts = await db.PostModel.extendPosts(posts, {
       visitor: {xsf: 9999},
       renderHTML: true,
@@ -16,8 +42,94 @@ router
       showAnonymousUser: true,
       url: true
     });
-    data.posts = posts;
+
+    const forums = await db.ForumModel.find({}, {displayName: 1, fid: 1});
+    const forumsObj = {};
+    forums.map(forum => forumsObj[forum.fid] = forum);
+
+    const results = [];
+
+    const threadsId = [];
+
+    for(const post of posts) {
+      if(post.type === 'thread') continue;
+      threadsId.push(post.tid);
+    }
+    const threads = await db.ThreadModel.find({tid: {$in: threadsId}}, {tid: 1, oc: 1});
+    const threadPosts = await db.PostModel.find({pid: {$in: threads.map(thread => thread.oc)}}, {tid: 1, t: 1});
+    const threadsObj = {};
+    for(const tp of threadPosts) {
+      const {tid, t} = tp;
+      threadsObj[tid] = {
+        tid,
+        t
+      }
+    }
+
+    for(const post of posts) {
+      if(post.type === 'post') {
+
+      }
+      let postType;
+      if(post.type === 'thread') {
+        postType = 'thread';
+      } else if(post.parentPostId) {
+        postType = 'comment';
+      } else {
+        postType = 'post';
+      }
+
+      const _forums = [];
+
+      for(const fid of post.mainForumsId) {
+        const forum = forumsObj[fid];
+        if(!forum) continue;
+        _forums.push(forum);
+      }
+
+      let thread;
+
+      if(post.type === 'thread') {
+        thread = {
+          tid: post.tid,
+          t: post.t
+        }
+      } else {
+        thread = threadsObj[post.tid];
+      }
+
+      const result = {
+        pid: post.pid,
+        toc: post.toc,
+        user: {
+          avatar: post.user.avatar,
+          uid: post.user.uid,
+          username: post.user.username
+        },
+        type: postType,
+        forums: _forums,
+        thread,
+        t: post.t,
+        c: post.c,
+        abstractCn: post.abstractCn,
+        abstractEn: post.abstractEn,
+        keyWordsCn: post.keyWordsCn,
+        keyWordsEn: post.keyWordsEn,
+        url: post.url,
+        mainForumsId: post.mainForumsId,
+        categoriesId: post.categoriesId
+      }
+
+      results.push(result);
+    }
+
+
+
+    data.posts = results;
     data.paging = paging;
+    data.t = t;
+    data.c = c;
+    data.nav = 'post';
     ctx.template = "nkc/post/post.pug";
     await next();
   });
