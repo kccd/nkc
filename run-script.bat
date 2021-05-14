995767cf9248d934f840c0b31ace9a86bfe947aa
tokisakiyuu <tokisakiyuu@qq.com>
2021-05-14 10:12:41 +0800

    fix: 修复用cmd执行run-script.bat脚本之后cmd进程直接退出的问题

--- run-script.bat
+++ run-script.bat
@@ -7,12 +7,12 @@ if "%~x1"==".js" (goto A) else if "%~x1"==".ts" (goto B) else (goto C)
 
 :A
 node %~f1
-exit
+exit /B
 
 :B
 node -r ts-node/register %~f1
-exit
+exit /B
 
 :C
 echo no found script file
-exit
+exit /B
