1522fe592b867da7a6d7c80ab1771b09771922a5
pengxiguaa <pengxiguaa@gmail.com>
2021-05-26 11:19:30 +0800

    fix: 修复视频文件名影响笔记高亮定位的问题

--- nkcModules/nkcRender/sources/article.js
+++ nkcModules/nkcRender/sources/article.js
@@ -52,8 +52,8 @@ module.exports = {
         <video class="plyr-dom" preload="none" controls="controls" poster="${poster}" data-rid="${rid}" data-plyr-title="${oname}">
           ${sourceHtml}
         </video>
+        <span class="nkcsource-video-title">${resource.oname} <span class="display-i-b text-danger" style="font-weight: 700">${getSize(resource.size)}</span></span>
       </span>
-      <span class="nkcsource-video-title">${resource.oname} <span class="display-i-b text-danger" style="font-weight: 700">${getSize(resource.size)}</span></span>
     `.trim();
     } else {
       return `<span data-tag="nkcsource" data-type="video-not-found"><span>视频已丢失（${oname}）</span></span>`
