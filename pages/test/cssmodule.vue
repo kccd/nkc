813fdb22f9b9270bf053373be19faf9e562f4038
tokisakiyuu <tokisakiyuu@qq.com>
2021-06-02 16:28:20 +0800

    feat: vue单文件组件css module case

---
+++ pages/test/cssmodule.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 :class="[$style.title, custom]" @click="change()">
+      Hello World
+      <a :class="$style.link" href="http://www.google.com">Google</a>
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    custom: ""
+  }),
+  methods: {
+    change() {
+      console.log(this.$style)
+      this.custom = this.$style.small;
+    }
+  }
+}
+</script>
+
+<style module scoped lang="less">
+  .title {
+    color: green;
+    transition: font-size .5s;
+    .link {
+      color: blue;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+  .small {
+    font-size: 1.5em;
+  }
+</style>
---
+++ pages/test/test.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import Cssmodule from "./cssmodule.vue";
+
+Vue.component("cssmodule", Cssmodule);
+
+new Vue({
+  el: "#app",
+  data: {
+
+  }
+});
