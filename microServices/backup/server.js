745e3c43e448f09abf7370ad579fc36266ce8000
pengxiguaa <pengxiguaa@gmail.com>
2021-06-25 14:54:07 +0800

    chore: 调整数据备份

--- microServices/backup/server.js
+++ microServices/backup/server.js
@@ -1,19 +1,13 @@
 require('colors');
-const crypto = require('crypto');
 const moment = require("moment");
 const PATH = require('path');
 const schedule = require('node-schedule');
 const archiver = require('archiver');
-const http = require('http');
+const fs = require('fs');
 const {spawn} = require('child_process');
 const {
   folder,
   time,
-  whitelist = [],
-  address,
-  port,
-  legalTime,
-  secret
 } = require('../../config/backup.json');
 const {
   database: dbName,
@@ -24,7 +18,12 @@ const {
 } = require('../../config/mongodb.json');
 
 
-schedule.scheduleJob(time, async () => {
+schedule.scheduleJob(time, backupDatabase);
+
+
+async function backupDatabase() {
+  const now = new Date();
+  const folderPath = getDataFolderByDate(now);
   console.log(`${getTimestamp()} 开始备份数据库 ${dbName} ...`);
   let data = '', error = '';
   const command = [
@@ -34,7 +33,7 @@ schedule.scheduleJob(time, async () => {
     '--db',
     dbName,
     '--out',
-    `${getDataFolderByDate()}`,
+    `${folderPath}`,
   ];
 
   if(dbUsername && dbPassword) {
@@ -65,90 +64,78 @@ schedule.scheduleJob(time, async () => {
     error += (d+'\n');
   });
   process.on('close', (code) => {
-    let info = '';
-    if (code === 0) {
-      info = `${getTimestamp()} 备份完成`;
-    } else {
-      info = `${getTimestamp()} 备份失败\n${error}`;
+    if(code !== 0) {
+      return console.log(`${getTimestamp()} 备份失败\n${error}`);
     }
-    console.log(info);
+    console.log(`${getTimestamp()} 备份完成 开始压缩...`);
+    const zipFilePath = getZipFilePathByDate(now);
+    compressedDir(PATH.resolve(folderPath, dbName), zipFilePath)
+      .then(() => {
+        console.log(`${getTimestamp()} 压缩完成 ${zipFilePath}`);
+      })
+      .catch(err => {
+        console.log(`${getTimestamp()} 压缩失败 ${err.message || err}`);
+      });
   });
-});
+}
 
+async function compressedDir(dirPath, zipFilePath) {
 
+  return new Promise((resolve, reject) => {
+    try{
+      const targetDirPath = PATH.dirname(zipFilePath);
+      fs.mkdirSync(targetDirPath, {
+        recursive: true
+      });
 
-const server = http.createServer((req, res) => {
-  const url = new URL(`http://test${req.url}`);
-  const token = url.searchParams.get('token');
-  const now = new Date();
-  const hour = now.getHours();
-  const [startingTime, endTime] = legalTime.split('-');
+      const zipFile = fs.createWriteStream(zipFilePath);
 
-  if(hour < Number(startingTime) || hour > Number(endTime)) {
-    return returnNull(res);
-  }
+      zipFile.on('error', (err) => {
+        reject(err);
+      });
+      zipFile.on('finish', () => {
+        resolve();
+      });
 
-  const ip = getIp(req);
+      const archive = archiver('zip', {
+        zlib: {level: 9}
+      });
+      archive.on('warning', function(err) {
+        if (err.code === 'ENOENT') {
+          console.log(err);
+        } else {
+          reject(err);
+        }
+      });
 
-  if(whitelist.length !== 0 && !whitelist.includes(ip)) {
-    return returnNull(res);
-  }
+      archive.on('error', function(err) {
+        reject(err);
+      });
 
-  const md5 = crypto.createHash('md5');
-  const result = md5.update(secret + moment(now).format("YYYYMMDD")).digest("hex");
-  if(token !== result) {
-    return returnNull(res);
-  }
+      archive.directory(dirPath, false);
 
-
-  const archive = archiver('zip', {
-    zlib: { level: 9 }
-  });
-
-  archive.on('warning', function(err) {
-    console.log(err);
-    if (err.code === 'ENOENT') {
-    } else {
-      res.statusCode = 500;
-      res.end(err.message);
+      archive.pipe(zipFile);
+      archive.finalize();
+    } catch (err) {
+      reject(err);
     }
-  });
+  })
 
-  archive.on('error', function(err) {
-    console.log(err);
-    res.statusCode = 500;
-    res.end(err.message);
-  });
-
-  archive.directory(getDataFolderByDate(new Date()), false);
-
-  archive.pipe(res);
-  archive.finalize();
-
-});
-
-server.listen(port, address, () => {
-  console.log(`backup server is running at ${address}:${port}`);
-  if(process.connected) process.send('ready');
-  process.on('message', function(msg) {
-    if (msg === 'shutdown') {
-      server.close();
-      console.log(`backup service stopped`.green);
-      process.exit(0);
-    }
-  });
-});
-
-
-function returnNull(res) {
-  res.statusCode = 404;
-  res.end();
 }
 
 function getDataFolderByDate(t) {
-  t = t || new Date();
   t = moment(t);
-  return PATH.resolve(folder, `./${t.format('YYYYMMDD')}`);
+  const folderPath =  PATH.resolve(folder, `./${dbName}/${t.format('YYYY')}/${t.format('MM')}/cache`);
+  fs.mkdirSync(folderPath, {
+    recursive: true
+  });
+  return folderPath
+}
+
+function getZipFilePathByDate(t) {
+  const folderPath = getDataFolderByDate(t);
+  t = moment(t);
+  return PATH.resolve(folderPath, `../${dbName}_${t.format('YYYYMMDD')}.zip`);
 }
 
 function getTimestamp(t) {
@@ -157,9 +144,4 @@ function getTimestamp(t) {
   return t.format('YYYY-MM-DD HH:mm:ss') + ' backup';
 }
 
-function getIp(req) {
-  return req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-}
+console.log(`数据库自动备份服务已启动`);
