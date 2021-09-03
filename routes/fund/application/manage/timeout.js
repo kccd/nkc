bf63ce96c252ef0e24868bb0951915bafb432a37
pengxiguaa <pengxiguaa@gmail.com>
2021-09-03 17:58:41 +0800

    chore: 优化基金

--- routes/fund/application/manage/timeout.js
+++ routes/fund/application/manage/timeout.js
@@ -3,6 +3,7 @@ router
   .post('/', async (ctx, next) => {
     const {data, state} = ctx;
     const {applicationForm} = data;
+    await fund.checkFundRole(state.uid, 'admin');
     await applicationForm.setUselessAsTimeout(state.uid);
     await next();
   })
