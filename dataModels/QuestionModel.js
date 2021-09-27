2194f7fc51a4f1a06afbc52a2a0e923efc6c585b
pengxiguaa <pengxiguaa@gmail.com>
2021-09-27 11:44:04 +0800

    fix: 修复考试系统图片无法加载的问题

--- dataModels/QuestionModel.js
+++ dataModels/QuestionModel.js
@@ -105,4 +105,23 @@ schema.statics.extendQuestions = async (questions) => {
   }));
 };
 
+/*
+* 保存试题的图片
+* @param {File} file 图片对象
+* @param {Number} qid 试题 ID
+* */
+schema.methods.updateImage = async function(file) {
+  const {path} = file;
+  const {_id} = this;
+  const {questionImagePath} = require('../settings/upload');
+  const {questionImageify} = require('../tools/imageMagick');
+  const targetPath = questionImagePath + '/' + _id + '.jpg';
+  await questionImageify(path, targetPath);
+  await this.updateOne({
+    $set: {
+      hasImage: true
+    }
+  });
+};
+
 module.exports = mongoose.model('questions', schema);
