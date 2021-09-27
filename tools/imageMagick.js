3f5f8e853fbd57ca4f276590d5a082b4d408767d
pengxiguaa <pengxiguaa@gmail.com>
2021-09-27 15:56:37 +0800

    chore: 优化题库图片上传自动填充白色背景

--- tools/imageMagick.js
+++ tools/imageMagick.js
@@ -487,7 +487,7 @@ const friendImageify = async (path, targetPath) => {
 const questionImageify = async (path, targetPath) => {
   const width = 500;
   const height = 250;
-  const arr = [path, '-resize', `${width}x${height}`, '-quality', '80', targetPath];
+  const arr = [path, '-background', 'white', '-flatten', '-resize', `${width}x${height}`, '-quality', '80', targetPath];
   if(linux) {
     return spawnProcess('convert', arr);
   } else {
