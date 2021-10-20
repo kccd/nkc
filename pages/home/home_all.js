b2b63d556f714954afd51123e2048f50ffc5f9c7
panbingjojo <2312032374@qq.com>
2021-10-20 14:28:22 +0800

    style: 修改主页排布方式

--- pages/home/home_all.js
+++ pages/home/home_all.js
@@ -1,5 +1,4 @@
 import Sortable from "sortablejs";
-const homeModal = new NKC.modules.homeModal();
 
 $(function() {
   // 轮播图
@@ -35,114 +34,64 @@ $(function() {
     }
   });
 });
-
-const homeAll = new Vue({
-  el: "#home_all",
-  data: {},
-  mounted() {
-    this.initSortable();
-  },
-  methods:{
-    initSortable() {
-      const masterContainerL = document.getElementsByClassName('home-categories-left')[0];
-      new Sortable(masterContainerL, {
-        group: 'master',
-        invertSwap: true,
-        handle: '.home-category-master-handle',
-        animation: 150,
-        fallbackOnBody: true,
-        swapThreshold: 0.65,
-        onEnd: this.changeOrder
-      });
-      const masterContainerR = document.getElementsByClassName('home-categories-right')[0];
-      new Sortable(masterContainerR, {
-        group: 'master',
-        invertSwap: true,
-        handle: '.home-category-master-handle',
-        animation: 150,
-        fallbackOnBody: true,
-        swapThreshold: 0.65,
-        onEnd: this.changeOrder
-      });
-    },
-    changeOrder(){
-      const domOrder = $('.home-category-master-handle');
-      const forumDoms = [];
-      for(let i = 0; i < domOrder.length; i++) {
-        const m = domOrder.eq(i);
-        const cid = Number(m.attr('data-cid'));
-        forumDoms.push({
-          cid,
-          order: i
-        });
-      }
-    },
-    //新建
-    modifyForum(){
-      homeModal.open(data => {
-        const name = data[0].value;
-        const description = data[1].value;
-        const warning = data[2].value;
-        const threadWarning = data[3].value;
-      }, {
-        title: '新建',
-        data: [
-          {
-            dom: 'input',
-            label: '标题',
-            value: ''
-          },
-          {
-            dom: 'textarea',
-            label: '介绍',
-            value: ''
-          },
-          {
-            dom: 'textarea',
-            label: '注意事项',
-            value: ''
-          },
-          {
-            dom: 'textarea',
-            label: '文章公告',
-            value: ''
-          }
-        ]
-      })
-    },
-    //编辑
-    editorForum(){
-      homeModal.open(data => {
-        const name = data[0].value;
-        const description = data[1].value;
-        const warning = data[2].value;
-        const threadWarning = data[3].value;
-      }, {
-        title: '编辑',
-        data: [
-          {
-            dom: 'input',
-            label: '标题',
-            value: ''
-          },
-          {
-            dom: 'textarea',
-            label: '介绍',
-            value: ''
-          },
-          {
-            dom: 'textarea',
-            label: '注意事项',
-            value: ''
-          },
-          {
-            dom: 'textarea',
-            label: '文章公告',
-            value: ''
-          }
-        ]
-      })
-    },
+function initSortable() {
+  const masterContainerL = document.getElementsByClassName('home-categories-left')[0];
+  new Sortable(masterContainerL, {
+    group: 'master',
+    invertSwap: true,
+    handle: '.home-category-master-handle',
+    animation: 150,
+    fallbackOnBody: true,
+    swapThreshold: 0.65,
+    onEnd: this.changeOrder
+  });
+  const masterContainerR = document.getElementsByClassName('home-categories-right')[0];
+  new Sortable(masterContainerR, {
+    group: 'master',
+    invertSwap: true,
+    handle: '.home-category-master-handle',
+    animation: 150,
+    fallbackOnBody: true,
+    swapThreshold: 0.65,
+    onEnd: this.changeOrder
+  });
+};
+function changeOrder(){
+  const domOrder = $('.home-category-master-handle');
+  const forumDoms = [];
+  for(let i = 0; i < domOrder.length; i++) {
+    const m = domOrder.eq(i);
+    const cid = Number(m.attr('data-cid'));
+    forumDoms.push({
+      cid,
+      order: i
+    });
   }
-})
+};
+//新建
+function modifyForum(){
 
+};
+//编辑
+function editorForum(cid){
+  const domBlock = $('.home-forums-list');
+  for(let i = 0;i<domBlock.length;i++){
+    const n = domBlock.eq(i);
+    const id = Number(n.attr('data-cid'));
+    if(id === Number(cid)){
+      this.domHidden = cid;
+    }
+  }
+
+};
+//删除
+function del(){};
+//屏蔽
+function disabled(){};
+function clickEditor(){
+
+};
+function editor(){
+  $('.home-title-r').css.visibility = 'initial';
+};
+function finished(){};
