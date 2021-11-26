06b0ba4b9a0c7c71edb28ec55b6e1acd46799e9c
pengxiguaa <pengxiguaa@gmail.com>
2021-11-26 14:57:25 +0800

    pref: 优化游客下载体验

--- pages/publicModules/downloadPanel/downloadPanel.js
+++ pages/publicModules/downloadPanel/downloadPanel.js
@@ -9,6 +9,7 @@ class DownloadPanel extends NKC.modules.DraggablePanel {
       data: {
         loading: true,
         error: false,
+        errorInfo: '',
         rid: '',
         resource: null,
         fileCountLimitInfo: [],
@@ -95,6 +96,7 @@ class DownloadPanel extends NKC.modules.DraggablePanel {
         reload() {
           this.loading = true;
           this.error = false;
+          this.errorInfo = '';
           this.getResourceInfo();
         },
         getResourceInfo() {
@@ -129,7 +131,7 @@ class DownloadPanel extends NKC.modules.DraggablePanel {
               console.log(err);
               this.loading = false;
               this.error = true;
-              sweetError(err);
+              this.errorInfo = err.error || err.message || err;
             })
         },
         open(rid) {
