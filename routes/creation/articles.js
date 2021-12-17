00464dd72977a0648ce272f65e6ae57d095c3473
pengxiguaa <pengxiguaa@gmail.com>
2021-12-17 14:07:22 +0800

    chore: 调整文档接口

--- routes/creation/articles.js
+++ routes/creation/articles.js
@@ -2,5 +2,12 @@ const router = require('koa-router')();
 router
   .get('/creator', async (ctx, next) => {
     await next();
+  })
+  .post('/creator', async (ctx, next) => {
+    const {body} = ctx;
+    const {files, fields} = body;
+    const article = JSON.parse(fields.article);
+    const {cover} = files;
+    await next();
   });
 module.exports = router;
