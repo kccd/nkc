8c82696ba280ca542e189bd9e97e65ba0958667d
’zhangbinGithub1‘ <13711880372@qq.com>
2021-12-28 17:53:44 +0800

    身份认证页面data数据过滤，认证设置页dom修改

--- pages/experimental/settings/auth/auth.js
+++ pages/experimental/settings/auth/auth.js
@@ -9,60 +9,60 @@ var app = new Vue({
     info: "",
     auditorIdStr: data.authSettings.auditorId.join(", ")
   },
-  mounted: function() {
-    if(this.certs.length > 0) {
+  mounted: function () {
+    if (this.certs.length > 0) {
       this.selectCertId = this.certs[0]._id;
     }
   },
   computed: {
-    certsObj: function() {
+    certsObj: function () {
       var obj = {};
-      for(var i = 0; i < this.certs.length; i++) {
+      for (var i = 0; i < this.certs.length; i++) {
         var cert = this.certs[i];
         obj[cert._id] = cert;
       }
       return obj;
     },
-    selectedCerts: function() {
+    selectedCerts: function () {
       var certsId = this.authSettings.auditorCerts;
       var arr = [];
-      for(var i = 0; i < certsId.length; i++) {
+      for (var i = 0; i < certsId.length; i++) {
         var certId = certsId[i];
         var cert = this.certsObj[certId];
-        if(cert) arr.push(cert);
+        if (cert) arr.push(cert);
       }
       return arr;
     }
   },
   methods: {
-    addCert:function() {
-      if(this.authSettings.auditorCerts.indexOf(this.selectCertId) === -1) {
+    addCert: function () {
+      if (this.authSettings.auditorCerts.indexOf(this.selectCertId) === -1) {
         this.authSettings.auditorCerts.push(this.selectCertId);
       }
     },
-    removeCert: function(id) {
+    removeCert: function (id) {
       var index = this.authSettings.auditorCerts.indexOf(id);
-      if(index !== -1) {
+      if (index !== -1) {
         this.authSettings.auditorCerts.splice(index, 1);
       }
     },
-    save: function() {
+    save: function () {
       var auditorIdStr = this.auditorIdStr;
       var arr = auditorIdStr.split(",");
       var auditorId = [];
-      for(var i = 0; i < arr.length; i++) {
+      for (var i = 0; i < arr.length; i++) {
         var id = arr[i];
         id = id.trim();
         auditorId.push(id);
       }
       this.authSettings.auditorId = auditorId;
       nkcAPI("/e/settings/auth", "PUT", {
-        authSettings: this.authSettings
-      })
-        .then(function() {
+          authSettings: this.authSettings
+        })
+        .then(function () {
           app.info = "保存成功";
         })
-        .catch(function(data) {
+        .catch(function (data) {
           app.error = data.error;
         })
     }
--- pages/user/settings/verify/verify.js
+++ pages/user/settings/verify/verify.js
@@ -2,136 +2,131 @@ import Vue from "vue";
 const DEFAULT_IMAGE = "/default/photo_setting.jpg";
 const UID = NKC.configs.uid;
 const vm = new Vue({
-	el: "#app",
-	data: {
-		...NKC.methods.getDataById('data').data || {},
-		IDCardAInputFile: null,
-		IDCardBInputFile: null,
-		videoInputFile: null,
-		videoCode: Math.floor(Math.random() * (9999 - 1000)) + 1000,
-		loading: false,
-		uploadProgress: '',
-		titles: []
-	},
-	mounted() {},
+  el: "#app",
+  data: {
+    ...(NKC.methods.getDataById("data").data || {}),
+    IDCardAInputFile: null,
+    IDCardBInputFile: null,
+    videoInputFile: null,
+    videoCode: Math.floor(Math.random() * (9999 - 1000)) + 1000,
+    loading: false,
+    uploadProgress: "",
+    titles: []
+  },
+  mounted() {},
 
-	computed: {
-		phoneNumberDesc(){
-			return this.auth1Content || '暂无提示，等待管理员添加。'
-		},
-		identityAuthDesc() {
-			return this.auth2Content || '暂无提示，等待管理员添加。'
-		},
-		verifyDescription() {
-			let {
-				videoCode,
-				auth3Content = ''
-			} = this;
-			return auth3Content.replace(/{code}/ig, videoCode);
-		},
-		IDCardA() {
-			if (this.IDCardAInputFile) {
-				return URL.createObjectURL(this.IDCardAInputFile);
-			}
-			const vid = this.authenticate.card.attachments[0];
-			return vid && ["in_review", "passed", "fail"].includes(this.authenticate.card.status) ?
-				`/u/${UID}/verifiedAssets/${vid}` :
-				DEFAULT_IMAGE;
-		},
-		IDCardB() {
-			if (this.IDCardBInputFile) {
-				return URL.createObjectURL(this.IDCardBInputFile);
-			}
-			const vid = this.authenticate.card.attachments[1];
-			return vid && ["in_review", "passed", "fail"].includes(this.authenticate.card.status) ?
-				`/u/${UID}/verifiedAssets/${vid}` :
-				DEFAULT_IMAGE;
-		},
-		verify2ExpiryDate() {
-			const date = new Date(this.authenticate.card.expiryDate || null);
-			return date.toLocaleDateString();
-		},
-		video() {
-			const {
-				videoInputFile
-			} = this;
-			if (videoInputFile) {
-				return URL.createObjectURL(videoInputFile);
-			}
-			const vid = this.authenticate.video.attachments[0];
-			return vid && ["in_review", "passed", "fail"].includes(this.authenticate.video.status) ?
-				`/u/${UID}/verifiedAssets/${vid}` :
-				DEFAULT_IMAGE;
-		},
-		verify3ExpiryDate() {
-			const date = new Date(this.authenticate.video.expiryDate || null);
-			return date.toLocaleDateString();
-		},
-	},
-	methods: {
-		videoUpdate(file) {
-			this.videoInputFile = file;
-			if (file.type !== "video/mp4") {
-				return sweetSuccess("视频上传成功")
-			}
-		},
-		IDCardAInputFileChange(file) {
-			this.IDCardAInputFile = file;
-			console.log(file);
-		},
-		IDCardBInputFileChange(file) {
-			this.IDCardBInputFile = file;
-			console.log(file);
-		},
-		async submitVerify2() {
-			const {
-				IDCardAInputFile,
-				IDCardBInputFile
-			} = this;
-			if (!IDCardAInputFile || !IDCardBInputFile) {
-				return sweetWarning("请先选择身份证正反两面2张照片后再试");
-			}
-			const form = new FormData();
-			form.append("surfaceA", IDCardAInputFile);
-			form.append("surfaceB", IDCardBInputFile);
-			try {
-				await nkcUploadFile("verify/verify2_form", "POST", form).then(() => {
-					return sweetSuccess("图片提交成功，请等待审核");
-				});
-			} catch (error) {
-				return sweetError(error);
-			}
-			this.authenticate.card.status = "in_review";
-		},
-		async submiteVerify3() {
-			const self = this;
-			const {
-				videoInputFile,
-				videoCode
-			} = self;
-			if (!videoInputFile) {
-				return sweetWarning("请先选择视频");
-			}
-			const form = new FormData();
-			form.append("video", videoInputFile);
-			form.append("code", videoCode);
-			try {
-				self.loading = true;
-				await nkcUploadFile("verify/verify3_form", "POST", form, function (e, progress) {
-					console.log(typeof progress);
-					self.uploadProgress = parseInt(progress);
-				}).then(() => {
-					self.uploadProgress = 100;
-					self.authenticate.video.status = "in_review";
-					self.loading = false;
-					return sweetSuccess("视频提交成功，请等待审核");
-				});
-			} catch (error) {
-				self.loading = false;
-				return sweetError(error);
-			}
-		}
-	}
+  computed: {
+    phoneNumberDesc() {
+      return this.auth1Content || "暂无提示，等待管理员添加。";
+    },
+    identityAuthDesc() {
+      return this.auth2Content || "暂无提示，等待管理员添加。";
+    },
+    verifyDescription() {
+      let { videoCode, auth3Content = "" } = this;
+      return auth3Content.replace(/{code}/gi, videoCode);
+    },
+    IDCardA() {
+      if (this.IDCardAInputFile) {
+        return URL.createObjectURL(this.IDCardAInputFile);
+      }
+      const vid = this.authenticate.card.attachments[0];
+      return vid &&
+        ["in_review", "passed", "fail"].includes(this.authenticate.card.status)
+        ? `/u/${UID}/verifiedAssets/${vid}`
+        : DEFAULT_IMAGE;
+    },
+    IDCardB() {
+      if (this.IDCardBInputFile) {
+        return URL.createObjectURL(this.IDCardBInputFile);
+      }
+      const vid = this.authenticate.card.attachments[1];
+      return vid &&
+        ["in_review", "passed", "fail"].includes(this.authenticate.card.status)
+        ? `/u/${UID}/verifiedAssets/${vid}`
+        : DEFAULT_IMAGE;
+    },
+    verify2ExpiryDate() {
+      const date = new Date(this.authenticate.card.expiryDate || null);
+      return date.toLocaleDateString();
+    },
+    video() {
+      const { videoInputFile } = this;
+      if (videoInputFile) {
+        return URL.createObjectURL(videoInputFile);
+      }
+      const vid = this.authenticate.video.attachments[0];
+      return vid &&
+        ["in_review", "passed", "fail"].includes(this.authenticate.video.status)
+        ? `/u/${UID}/verifiedAssets/${vid}`
+        : DEFAULT_IMAGE;
+    },
+    verify3ExpiryDate() {
+      const date = new Date(this.authenticate.video.expiryDate || null);
+      return date.toLocaleDateString();
+    }
+  },
+  methods: {
+    videoUpdate(file) {
+      this.videoInputFile = file;
+      if (file.type !== "video/mp4") {
+        return sweetSuccess("视频上传成功");
+      }
+    },
+    IDCardAInputFileChange(file) {
+      this.IDCardAInputFile = file;
+      console.log(file);
+    },
+    IDCardBInputFileChange(file) {
+      this.IDCardBInputFile = file;
+      console.log(file);
+    },
+    async submitVerify2() {
+      const { IDCardAInputFile, IDCardBInputFile } = this;
+      if (!IDCardAInputFile || !IDCardBInputFile) {
+        return sweetWarning("请先选择身份证正反两面2张照片后再试");
+      }
+      const form = new FormData();
+      form.append("surfaceA", IDCardAInputFile);
+      form.append("surfaceB", IDCardBInputFile);
+      try {
+        await nkcUploadFile("verify/verify2_form", "POST", form).then(() => {
+          return sweetSuccess("图片提交成功，请等待审核");
+        });
+      } catch (error) {
+        return sweetError(error);
+      }
+      this.authenticate.card.status = "in_review";
+    },
+    async submiteVerify3() {
+      const self = this;
+      const { videoInputFile, videoCode } = self;
+      if (!videoInputFile) {
+        return sweetWarning("请先选择视频");
+      }
+      const form = new FormData();
+      form.append("video", videoInputFile);
+      form.append("code", videoCode);
+      try {
+        self.loading = true;
+        await nkcUploadFile("verify/verify3_form", "POST", form, function(
+          e,
+          progress
+        ) {
+          console.log(typeof progress);
+          self.uploadProgress = parseInt(progress);
+        }).then(() => {
+          self.uploadProgress = 100;
+          self.authenticate.video.status = "in_review";
+          self.loading = false;
+          return sweetSuccess("视频提交成功，请等待审核");
+        });
+      } catch (error) {
+        self.loading = false;
+        return sweetError(error);
+      }
+    }
+  }
 });
 
 console.log(vm);
@@ -142,61 +137,60 @@ console.log(vm);
 // });
 
 function initEvent(elementId) {
-	$('#' + elementId).on('change', function () {
-		var file = $('#' + elementId)[0].files[0];
-		var formData = new FormData();
-		formData.append('file', file);
-		formData.append('photoType', elementId);
-		postUpload('/photo', formData, function () {
-			window.location.reload();
-		});
-	});
+  $("#" + elementId).on("change", function() {
+    var file = $("#" + elementId)[0].files[0];
+    var formData = new FormData();
+    formData.append("file", file);
+    formData.append("photoType", elementId);
+    postUpload("/photo", formData, function() {
+      window.location.reload();
+    });
+  });
 }
 
 function submitAuth(uid, number) {
-	nkcAPI('/u/' + uid + '/auth/' + number, 'POST', {})
-		.then(function (data) {
-			screenTopAlert('提交成功，请耐心等待审核');
-			setTimeout(function () {
-				window.location.reload()
-			}, 2000)
-		})
-		.catch(function (data) {
-			screenTopWarning(data.error);
-		})
+  nkcAPI("/u/" + uid + "/auth/" + number, "POST", {})
+    .then(function(data) {
+      screenTopAlert("提交成功，请耐心等待审核");
+      setTimeout(function() {
+        window.location.reload();
+      }, 2000);
+    })
+    .catch(function(data) {
+      screenTopWarning(data.error);
+    });
 }
 
-
 function unSubmitAuth(uid, number) {
-	nkcAPI('/u/' + uid + '/auth?number=' + number, 'DELETE', {})
-		.then(function () {
-			screenTopAlert('撤销成功');
-			window.location.reload();
-		})
-		.catch(function (data) {
-			screenTopWarning(data.error);
-		})
+  nkcAPI("/u/" + uid + "/auth?number=" + number, "DELETE", {})
+    .then(function() {
+      screenTopAlert("撤销成功");
+      window.location.reload();
+    })
+    .catch(function(data) {
+      screenTopWarning(data.error);
+    });
 }
 
 Object.assign(window, {
-	initEvent,
-	submitAuth,
-	unSubmitAuth,
+  initEvent,
+  submitAuth,
+  unSubmitAuth
 });
-
 
 // 上传认证3视频
 $("#pickVerify3Video").on("click", () => {
-	$("#handheldIdCard").one("change", ({
-		target
-	}) => {
-		const file = target.files[0];
-		$(".idcard-video > video").attr("src", URL.createObjectURL(file))
-		var formData = new FormData();
-		formData.append("file", file);
-		nkcUploadFile("verify/verify3_upload_video", "POST", formData, function (event, percentage) {
-			console.log(percentage);
-		});
-	});
-	$("#handheldIdCard").trigger("click");
+  $("#handheldIdCard").one("change", ({ target }) => {
+    const file = target.files[0];
+    $(".idcard-video > video").attr("src", URL.createObjectURL(file));
+    var formData = new FormData();
+    formData.append("file", file);
+    nkcUploadFile("verify/verify3_upload_video", "POST", formData, function(
+      event,
+      percentage
+    ) {
+      console.log(percentage);
+    });
+  });
+  $("#handheldIdCard").trigger("click");
 });
