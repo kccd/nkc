bc63b77afd7217e9fd593d65edd73e1938ec2b61
pengxiguaa <pengxiguaa@gmail.com>
2022-01-11 18:09:40 +0800

    pref: document 保存历史同时保存笔记选区

--- dataModels/DocumentModel.js
+++ dataModels/DocumentModel.js
@@ -240,6 +240,7 @@ schema.statics.createBetaDocument = async (props) => {
 * */
 schema.methods.copyToHistoryDocument = async function() {
   const DocumentModel = mongoose.model('documents');
+  const NoteModel = mongoose.model('notes');
   const originDocument = this.toObject();
   delete originDocument._v;
   originDocument.type = 'history';
@@ -247,6 +248,7 @@ schema.methods.copyToHistoryDocument = async function() {
   originDocument.toc = new Date();
   const document = DocumentModel(originDocument);
   await document.save();
+  await NoteModel.copyDocumentNoteAndUpdateNewNoteTargetId(this._id, document._id);
   return document;
 };
 
@@ -281,10 +283,8 @@ schema.methods.setAsHistoryDocument = async function() {
 schema.statics.createBetaDocumentByStableDocument = async function(did) {
   const DocumentModel = mongoose.model('documents');
   const NoteModel = mongoose.model('notes');
-  const SettingModel = mongoose.model('settings');
   const stableDocument = await DocumentModel.findOne({did, type: 'stable'});
   if(!stableDocument) throwErr(500, `文档 ${did} 不存在正式版，无法生成编辑版`);
-  const stableNotes = await NoteModel.getNotesByDocId(stableDocument._id);
   const originDocument = stableDocument.toObject();
   delete originDocument._v;
   originDocument.type = 'beta';
@@ -292,20 +292,7 @@ schema.statics.createBetaDocumentByStableDocument = async function(did) {
   originDocument.toc = new Date();
   const betaDocument = DocumentModel(originDocument);
   await betaDocument.save();
-  // 复制笔记选择等信息
-  for(const note of stableNotes) {
-    let newNote = note.toObject();
-    delete newNote._id;
-    delete newNote.__v;
-    newNote._id = await SettingModel.operateSystemID('notes', 1);
-    newNote = NoteModel(newNote);
-    newNote.save();
-    await note.updateOne({
-      $set: {
-        targetId: betaDocument._id
-      }
-    });
-  }
+  await NoteModel.copyDocumentNoteAndUpdateOriginNoteTargetId(stableDocument._id, betaDocument._id);
   return betaDocument;
 };
 
