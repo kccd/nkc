ee24c7233f05fe1e7f77657d59612453cf8a5ebd
pengxiguaa <pengxiguaa@gmail.com>
2022-02-21 17:10:03 +0800

    chore: 调整独立文章

--- routes/creation/article.js
+++ routes/creation/article.js
@@ -1,23 +1,25 @@
 const router = require('koa-router')();
 router
-.get('/', async (ctx, next) => {
-  //获取独立文章信息
-  const {query, state, db, data} = ctx;
-  const {aid, mid} = query;
-  const article = await db.ArticleModle.findOnly({_id: aid});
-  if(!article) ctx.throw(400, '未找到article,请刷新后重试');
-  if(article.status === 'deleted') ctx.throw(403, '权限不足');
-  const document = await article.getDocumentForArticle();
-  data.editorInfo = {
-    document,
-    article
-  };
-  data.articleId = aid;
-  // const match = {
-  //   uid: state.uid,
-  //   source: 'column',
-  //   published: true,
-  //   deleted: false,
-  // };
-  await next();
+  .get('/', async (ctx, next) => {
+    //获取独立文章信息
+    const {query, state, db, data} = ctx;
+    const {aid, mid} = query;
+    const article = await db.ArticleModle.findOnly({_id: aid});
+    if(!article) ctx.throw(400, '未找到article,请刷新后重试');
+    if(article.status === 'deleted') ctx.throw(403, '权限不足');
+    const document = await article.getDocumentForArticle();
+    data.editorInfo = {
+      document,
+      article
+    };
+    data.articleId = aid;
+    // const match = {
+    //   uid: state.uid,
+    //   source: 'column',
+    //   published: true,
+    //   deleted: false,
+    // };
+    await next();
 })
+
+module.exports = router;
