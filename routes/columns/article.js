a3127fcdbb3e9e6d1ef69a66d2c5fe4cf5a994a1
’zhangbinGithub1‘ <13711880372@qq.com>
2022-02-23 18:25:13 +0800

    feat 专栏文章显示进一步完善

--- routes/columns/article.js
+++ routes/columns/article.js
@@ -3,22 +3,9 @@ router.get('/:aid', async (ctx, next)=>{
   const {db, data, nkcModules, params} = ctx;
   ctx.template = 'columns/article.pug';
   const {_id, aid} = params;
-  let article = await db.ColumnPostModel.getArticleById(_id, aid);
-  article = article.toObject()
-  const allowKey = ['t','c','abstratCn','abstratEn','keyWordsCn','keyWordsEn'];
-  let articleObj = {} 
-  // console.log(article,'article')
-  for (const key in article) {
-    if (Object.hasOwnProperty.call(article, key)) {
-      console.log(key,allowKey.includes(key))
-      if(allowKey.includes(key)){
-        const item = article[key];
-        articleObj[key] = item
-      }
-    }
-  }
-  data.article = articleObj;
-  console.log(articleObj,'articleObj',articleObj.keyWordsCn,'keyWordsCn')
+  let article = await db.ColumnPostModel.getRequiredData(_id, aid);
+  console.log(article,'article')
+  data.article = article
   await next();
 })
 module.exports = router;
