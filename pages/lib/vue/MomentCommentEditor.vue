c3f3b7fbc9e139773b0dfc8be93e95d70dcba81a
pengxiguaa <pengxiguaa@gmail.com>
2022-03-09 14:52:01 +0800

    chore: 动态评论编辑器

--- pages/lib/vue/MomentCommentEditor.vue
+++ pages/lib/vue/MomentCommentEditor.vue
@@ -1,10 +1,32 @@
 <template lang="pug">
   .moment-comment-editor 动态评论编辑器
-    textarea-editor(:placeholder="placeholder" )
+    .moment-comment-editor-container
+      .moment-comment-textaea-ditor-container
+        textarea-editor(:placeholder="placeholder" height="2rem")
+      .moment-comment-editor-button
+        button.btn.btn-default.btn-sm 提交
 </template>
 
 <style lang="less" scoped>
-
+  .moment-comment-editor{
+    @buttonContainerWidth: 4rem;
+    @buttonContainerHeight: 3rem;
+    .moment-comment-editor-container{
+      position: relative;
+      padding-right: @buttonContainerWidth;
+      min-height: @buttonContainerHeight;
+      .moment-comment-editor-button{
+        position: absolute;
+        height: 100%;
+        top: 0;
+        right: 0;
+        button{
+          height: 100%;
+          width: 100%;
+        }
+      }
+    }
+  }
 </style>
 
 <script>
