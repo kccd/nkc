97b5be2c6007f8b9602fcb6f32c6cc6142fd4e14
changbin <1694525125@qq.com>
2022-03-30 17:44:33 +0800

    feat:关注用户内容

--- pages/spa/views/user/subscribe/SubscribeUsers.vue
+++ pages/spa/views/user/subscribe/SubscribeUsers.vue
@@ -1,21 +1,42 @@
 <template lang="pug">
   //关注的用户
   .subscribe-user
-    h4 这是我关注的用户
+    .subscribe-types
+    .subscribe-type-edit 管理分类
+    div 主分类：
+      div
+        button.subscribe-type 全部
+      div(v-for="t in subscribeTypes" :key=t)
+        button.subscribe-type {{t}}
+
+
 </template>
 <style lang="less">
 
 </style>
 <script>
+import {nkcAPI} from "../../../../lib/js/netAPI";
+import {getState} from "../../../../lib/js/state";
 export default {
-  data: () => ({
-
+  data: () =>({
+    uid: NKC.configs.uid,
+    users: [],
+    subscribeTypes:['XIAO','垃圾']
   }),
   components: {
 
   },
   methods: {
-
+    getUser() {
+      const self = this;
+      nkcAPI(`/u/${self.uid}/s/user`, 'GET')
+      .then((res) => {
+        self.users = res.users;
+      })
+      .catch(err => {
+        sweetError(err);
+      })
+    }
   }
 }
 </script>
