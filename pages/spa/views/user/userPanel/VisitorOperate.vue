f68e4bb1e1387cfd839f13047352f6be33eb6acf
changbin <1694525125@qq.com>
2022-04-14 16:02:42 +0800

    feat:主页举报功能

--- pages/spa/views/user/userPanel/VisitorOperate.vue
+++ pages/spa/views/user/userPanel/VisitorOperate.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   .visitor-operate
+    complaint(ref="complaint")
     button.m-b-05.m-r-05.btn-sm.btn-default.btn(@click="blackListOperation") {{usersBlUidList.indexOf(targetUser.uid,1) === -1 ? '加入黑名单' : '移出黑名单'}}
-    button.m-b-05.m-r-05.btn-sm.btn-default.btn 举报
+    button.m-b-05.m-r-05.btn-sm.btn-default.btn(@click="userComplaint") 举报
 </template>
 <style lang="less" scoped>
 @import "../../../../publicModules/base";
@@ -10,12 +11,15 @@
 <script>
 import {nkcAPI} from "../../../../lib/js/netAPI";
 import {EventBus} from "../../../eventBus";
-
+import Complaint from "../../../../lib/vue/Complaint";
 export default {
   props: ["usersBlUid", "targetUser"],
   data: () => ({
     usersBlUidList: [],
   }),
+  components:{
+    "complaint":Complaint
+  },
   created() {
     this.usersBlUidList = this.usersBlUid
     EventBus.$on('removeToBl',(uid)=>{
@@ -25,6 +29,11 @@ export default {
   mounted() {
   },
   methods: {
+    //举报
+    userComplaint(){
+      this.$refs.complaint.open('user',this.targetUser.uid)
+    },
+    //拉黑按钮操作
     blackListOperation(){
       const bool = this.usersBlUidList.indexOf(this.targetUser.uid,1) === -1;
       if(bool) this.addUserToBlackList(this.targetUser.uid,"userHome")
