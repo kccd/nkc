91a58b27b2713f1880d1c8a1c87e10ebae5dbfd8
‘panbingjojo’ <2312032374@qq.com>
2022-04-14 15:13:28 +0800

    fix: 修复最新文章链接问题

--- pages/spa/views/user/propfile/profile/Profile.vue
+++ pages/spa/views/user/propfile/profile/Profile.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   .div
     .paging-button
-      a.radius-left.button(@click="toRoute('moment')" :class="t === 'moment'?'active':''") 动态
-      a.button(@click="toRoute('post')" :class="t === 'post'?'active':''") 回复
-      a.button(@click="toRoute('thread')" :class="t === 'thread'?'active':''") 文章
-      a.button(@click="toRoute('follower')" :class="t === 'follower' ? 'active' : ''") 关注
-      a.radius-right.button(@click="toRoute('fan')" :class="t === 'fan' ? 'active' : ''") 粉丝
-    .post-panel-item
+      //a.radius-left.button(@click="toRoute('moment')" :class="t === 'moment'?'active':''") 动态
+      a.button(@click="toRoute('post')" :class="t === 'post'?'active':''" v-if="t ==='post' || t === 'thread'") 回复
+      a.button(@click="toRoute('thread')" :class="t === 'thread'?'active':''" v-if="t ==='post' || t === 'thread'") 文章
+      a.button(@click="toRoute('follower')" :class="t === 'follower' ? 'active' : ''" v-if="t ==='follower' || t === 'fan'") 关注
+      a.radius-right.button(@click="toRoute('fan')" :class="t === 'fan' ? 'active' : ''" v-if="t ==='follower' || t === 'fan'") 粉丝
+    .post-panel-item.p-b-1
       router-view
 </template>
 <style lang="less" scoped>
