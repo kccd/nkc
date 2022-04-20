0012a019cf66b7472b59f0b4483ee17bc75d59bf
pengxiguaa <pengxiguaa@gmail.com>
2022-04-20 19:03:24 +0800

    pref: 优化app本地账户更新

--- routes/app/account/index.js
+++ routes/app/account/index.js
@@ -5,18 +5,23 @@ router
     const {user} = data;
     data.userInfo = null;
     if(state.uid) {
-      const userCode = await db.UserModel.getCode(state.uid);
+      const userCodes = await db.UserModel.getCode(state.uid);
       const extendedUser = await db.UserModel.extendUserInfo(user);
       const userGrade = extendedUser.grade;
+
+      const subUsersId = await db.SubscribeModel.getUserSubUsersId(state.uid);
+      const fansId = await db.SubscribeModel.getUserFansId(state.uid);
+
       data.userInfo = {
         uid: user.uid,
         username: user.username,
         avatarUrl: nkcModules.tools.getUrl('userAvatar', user.avatar),
+        homeUrl: nkcModules.tools.getUrl('userHome', user.uid),
         certsName: extendedUser.info.certsName,
-        code: userCode,
+        codes: userCodes,
         xsf: user.xsf,
-        followers: 0,
-        following: 0,
+        followers: fansId.length,
+        following: subUsersId.length,
         gradeId: userGrade._id,
         gradeName: userGrade.displayName,
         gradeIconUrl: nkcModules.tools.getUrl('gradeIcon', userGrade._id),
