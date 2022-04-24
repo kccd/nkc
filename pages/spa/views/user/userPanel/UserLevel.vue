52e0a8ae6092a5563b257ad7808368f98c2652f6
changbin <1694525125@qq.com>
2022-04-24 17:20:34 +0800

    style:用户名样式，banner蒙层

--- pages/spa/views/user/userPanel/UserLevel.vue
+++ pages/spa/views/user/userPanel/UserLevel.vue
@@ -5,10 +5,17 @@
     span {{targetUser.info.certsName}}
 </template>
 <style lang="less" scoped>
+.account-user-level{
+  font-size: 1rem;
+}
 .user-level-icon {
   height: 1.2rem;
   width: 1.2rem;
   margin-right: 0.5rem;
+  @media (max-width: 768px){
+    height: 1rem;
+    width: 1rem;
+  }
 }
 </style>
 
