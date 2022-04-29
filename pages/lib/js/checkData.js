c1e71559ff61463a10fa3c5380c5b2e7e66f0af9
changbin <1694525125@qq.com>
2022-04-29 15:43:03 +0800

    fix:部分用户验证动态码失败

--- pages/lib/js/checkData.js
+++ pages/lib/js/checkData.js
@@ -1,3 +1,5 @@
+import {sweetWarning} from "./sweetAlert";
+
 export const checkString = NKC.methods.checkData.checkString;
 export const checkNumber = NKC.methods.checkData.checkNumber;
 export const getLength = NKC.methods.checkData.getLength;
@@ -8,7 +10,13 @@ export const getLength = NKC.methods.checkData.getLength;
 export const checkUserCode = function (uid) {
   return sweetPrompt('请输入动态码')
     .then(code => {
-      return nkcAPI(`/u/${data.uid}/code`, 'POST', {code})
+      if(code && code.length > 0){
+        return nkcAPI(`/u/${uid}/code`, 'POST', {code})
+      }else {
+        return new Promise((resolve,reject)=>{
+          reject('动态码不能为空')
+        })
+      }
     })
     .then(() => {
       sweetSuccess('验证通过');
