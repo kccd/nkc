513d5b60055cf63ca50f2e607ee51b670c5f0391
’zhangbinGithub1‘ <13711880372@qq.com>
2022-05-09 11:04:54 +0800

    feat: 媒体资源删除图片，新增、删除、编辑分组添加socket

--- routes/me/index.js
+++ routes/me/index.js
@@ -13,7 +13,7 @@ meRouter
     //获取资源选择/资源管理
     const {user} = ctx.data;
     const {db, data, nkcModules, state} = ctx;
-    let {quota, skip, type, c, resourceCategories} = ctx.query;
+    let {quota, skip, type, c, resourceCategories, reqType} = ctx.query;
     let queryMap;
     const {uid} = state;
     if(!c) c = "all";
@@ -47,30 +47,39 @@ meRouter
       queryMap.cid = resourceCategories;
     }
     let newSkip = quota * skip;
-    let mediaCount = await db.ResourceModel.find(queryMap).countDocuments();
-    //获取用户自定义资源分类
-    let categories = await db.ResourceCategoryModel.find({uid}).sort({order: 1});
-    data.categories = await db.ResourceCategoryModel.extendCount(categories);
-    data.paging = nkcModules.apiFunction.paging(skip, mediaCount, quota);
-    let maxSkip = Math.ceil(mediaCount / quota);
-    if(maxSkip < 1) maxSkip = 1;
-    if(skip >= maxSkip){
-      ctx.data.skip = maxSkip - 1;
+    // 只需要分组资源数据
+    if(reqType === 'resources') {
+      data.resources = await db.ResourceModel.find(queryMap).sort({toc: -1}).skip(newSkip).limit(quota);
+      // 只需要分组信息
+    }else if (reqType === 'group') {
+      let categories = await db.ResourceCategoryModel.find({uid}).sort({order: 1});
+      data.categories = await db.ResourceCategoryModel.extendCount(categories);
     }else{
-      ctx.data.skip = skip;
+      let mediaCount = await db.ResourceModel.find(queryMap).countDocuments();
+      //获取用户自定义资源分类
+      let categories = await db.ResourceCategoryModel.find({uid}).sort({order: 1});
+      data.categories = await db.ResourceCategoryModel.extendCount(categories);
+      data.paging = nkcModules.apiFunction.paging(skip, mediaCount, quota);
+      let maxSkip = Math.ceil(mediaCount / quota);
+      if(maxSkip < 1) maxSkip = 1;
+      if(skip >= maxSkip){
+        ctx.data.skip = maxSkip - 1;
+      }else{
+        ctx.data.skip = skip;
+      }
+      if(newSkip > mediaCount) {
+        ctx.throw(400, '已经是最后一页了')
+      }
+      ctx.data.maxSkip = maxSkip;
+      ctx.data.count = {
+        ungroupedCount: await db.ResourceModel.countDocuments({del: false, cid: '', uid: user.uid, del: false, type: 'resource'}),
+        allCount: await db.ResourceModel.countDocuments({del: false, uid: user.uid, type: 'resource'}),
+        trashCount: await db.ResourceModel.countDocuments({del: true, del: true, uid: user.uid, type: 'resource'}),
+      }
+      ctx.data.resources = await db.ResourceModel.find(queryMap).sort({toc: -1}).skip(newSkip).limit(quota);
+      const uploadSettings = await db.SettingModel.getSettings('upload');
+      data.sizeLimit = uploadSettings.sizeLimit;
     }
-    if(newSkip > mediaCount) {
-      ctx.throw(400, '已经是最后一页了')
-    }
-    ctx.data.maxSkip = maxSkip;
-    ctx.data.count = {
-      ungroupedCount: await db.ResourceModel.countDocuments({del: false, cid: '', uid: user.uid, del: false, type: 'resource'}),
-      allCount: await db.ResourceModel.countDocuments({del: false, uid: user.uid, type: 'resource'}),
-      trashCount: await db.ResourceModel.countDocuments({del: true, del: true, uid: user.uid, type: 'resource'}),
-    }
-    ctx.data.resources = await db.ResourceModel.find(queryMap).sort({toc: -1}).skip(newSkip).limit(quota);
-    const uploadSettings = await db.SettingModel.getSettings('upload');
-    data.sizeLimit = uploadSettings.sizeLimit;
     await next();
   })
 	.get('/life_photos', async (ctx, next) => {
