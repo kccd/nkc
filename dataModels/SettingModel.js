4eb46500811672b3e247aa9b4b3a71fb09a0e716
PXG <pengxiguaa@gmail.com>
2022-05-09 15:16:29 +0800

    fix: APP侧滑面板管理图标不显示

--- dataModels/SettingModel.js
+++ dataModels/SettingModel.js
@@ -976,12 +976,13 @@ settingSchema.statics.getManagementData = async (user) => {
   const ComplaintModel = mongoose.model('complaints');
   const ProblemModel = mongoose.model('problems');
   const results = [];
-  
+
   if(await user.hasPermission('nkcManagement')) {
     results.push({
       name: '前台管理',
       url: '/nkc',
       icon: 'ft-layers',
+      webIcon: 'fa-cogs',
       count: 0,
     });
   }
@@ -990,6 +991,7 @@ settingSchema.statics.getManagementData = async (user) => {
       name: '后台管理',
       url: '/e',
       icon: 'i-server',
+      webIcon: 'fa-cogs',
       count: 0
     });
   }
@@ -1028,6 +1030,7 @@ settingSchema.statics.getManagementData = async (user) => {
       name: '内容审核',
       url: '/review',
       icon: 'fs-preview',
+      webIcon: 'fa-shield',
       count
     });
   }
@@ -1036,6 +1039,7 @@ settingSchema.statics.getManagementData = async (user) => {
       name: '投诉列表',
       url: '/complaint',
       icon: 'ft-alert-triangle',
+      webIcon: 'fa-minus-circle',
       count: await ComplaintModel.countDocuments({resolved: false})
     });
   }
@@ -1044,6 +1048,7 @@ settingSchema.statics.getManagementData = async (user) => {
       name: '问题列表',
       url: '/problem/list',
       icon: 'e-new',
+      webIcon: 'fa-exclamation-circle',
       count: await ProblemModel.countDocuments({resolved: false}),
     });
   }
@@ -1052,6 +1057,7 @@ settingSchema.statics.getManagementData = async (user) => {
       name: '文库记录',
       url: '/libraries/logs',
       icon: 'i-library',
+      webIcon: 'fa-book',
       count: 0
     });
   }
