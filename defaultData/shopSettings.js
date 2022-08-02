b62d2d67392a0dbee6e08428d541e7cb6ed46d27
pengxiguaa <pengxiguaa@gmail.com>
2022-08-02 10:48:36 +0800

    fix: 后台商品禁售列表不显示、app订单页返回异常

---
+++ defaultData/shopSettings.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    // 设置类型
+    type: "homeSetting",
+    // 轮播图
+    carousels: [],
+    // 首页精选
+    featureds: [],
+    // 店铺推荐
+    recommendations: [],
+    // 最热商品
+    populars: [],
+    // 权限申请身份等级
+    authLevel: 1,
+    // 封禁名单
+    banList: []
+  }
+]
