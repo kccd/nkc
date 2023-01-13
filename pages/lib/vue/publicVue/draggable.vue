07496f8ed860c60e487297fc3008df015a37a8f0
changbin123 <1694525125@qq.com>
2023-01-13 10:12:09 +0800

    pref:优化文章选择器拖动框关闭

--- pages/lib/vue/publicVue/draggable.vue
+++ pages/lib/vue/publicVue/draggable.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .module-dialog-body
-    .module-dialog-header(ref="draggableHandle")
-      .module-dialog-title {{title}}
+    .module-dialog-header
+      .module-dialog-title(ref="draggableHandle") {{title}}
       .module-dialog-close(@click="close")
         .fa.fa-remove
     .module-dialog-content
@@ -31,6 +31,7 @@
     height: 3rem;
     line-height: 3rem;
     background-color: #f6f6f6;
+    padding-right: 3rem;
     .module-dialog-close{
       cursor: pointer;
       color: #aaa;
