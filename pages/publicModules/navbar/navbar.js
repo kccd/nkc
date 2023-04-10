223d4ced58511dce2b67db97ee55d9b100612d5b
916306719 <916306719@qq.com>
2023-04-10 11:19:05 +0800

    feat:弹出框功能优化

--- pages/publicModules/navbar/navbar.js
+++
@@ -1,9 +0,0 @@
-export function openMessageCenter() {
-  const width = localStorage.getItem('windowWidth');
-  const height = localStorage.getItem('windowHeight');
-  window.open(
-    '/message',
-    'Message',
-    `location=no,width=${width},height=${height}`,
-  );
-}
