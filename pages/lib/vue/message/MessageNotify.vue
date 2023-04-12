49297f11f043bdb272a7cdd4f13ee1e3b3aa1581
916306719 <916306719@qq.com>
2023-04-12 10:50:40 +0800

    fix:聊天的socket的功能优化

---
+++ pages/lib/vue/message/MessageNotify.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+export default {
+  data: () =>({
+  }),
+  mounted() {
+    console.log(111111);
+  }
+
+}
+</script>
+<style scoped lang="less"></style>
