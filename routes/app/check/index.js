656be76a028eb6247ceccc009423310b38ce64f7
pxgo <pengxiguaa@gmail.com>
2023-05-09 15:31:25 +0800

    chore: remove unused code

--- routes/app/check/index.js
+++ routes/app/check/index.js
@@ -1,38 +1,49 @@
 const Router = require('koa-router');
 const config = require('../../../config/server.json');
 const checkRouter = new Router();
-checkRouter
-  .get('/', async (ctx, next) => {
-    const { data, db, query, state} = ctx;
+checkRouter.get('/', async (ctx, next) => {
+  const { data, db, query, state } = ctx;
+  /*
+    此处为了兼容旧版APP，新平台不再需要
     if(ctx.req.headers.cookie) {
       data.cookie = Buffer.from(ctx.req.headers.cookie).toString('base64');
     }
-    let { systemType, sysType, version} = query;
-    if(!systemType) systemType = sysType;
-    if (systemType) {
-      const queryMap = {
-        appPlatForm: systemType,
-        stable: true,
-        disabled: false
-      };
-      const latestVer = await db.AppVersionModel.findOne(queryMap);
-      if (latestVer) {
-        const newVersion = latestVer.toObject();
-        newVersion.url = config.domain + '/app/' + newVersion.appPlatForm + '/' + newVersion.hash + "?t=" + Date.now();
-        if(version && version !== newVersion.appVersion) {
-          data.newVersion = newVersion;
-        }
-        data.latestVer = newVersion; // 兼容旧版APP的下载链接 2020-1-18，APP更新多个版本后可移除
+    */
+  let { systemType, sysType, version } = query;
+  if (!systemType) {
+    systemType = sysType;
+  }
+  if (systemType) {
+    const queryMap = {
+      appPlatForm: systemType,
+      stable: true,
+      disabled: false,
+    };
+    const latestVer = await db.AppVersionModel.findOne(queryMap);
+    if (latestVer) {
+      const newVersion = latestVer.toObject();
+      newVersion.url =
+        config.domain +
+        '/app/' +
+        newVersion.appPlatForm +
+        '/' +
+        newVersion.hash +
+        '?t=' +
+        Date.now();
+      if (version && version !== newVersion.appVersion) {
+        data.newVersion = newVersion;
       }
+      data.latestVer = newVersion; // 兼容旧版APP的下载链接 2020-1-18，APP更新多个版本后可移除
     }
+  }
 
-    /*
-    * app检测服务器地址是否正常工作
-    * 服务器收到请求后会将query中的origin原样返回给app
-    * */
-    const {origin} = query;
-    data.origin = origin;
+  /*
+   * app检测服务器地址是否正常工作
+   * 服务器收到请求后会将query中的origin原样返回给app
+   * */
+  const { origin } = query;
+  data.origin = origin;
 
-    await next();
-  });
+  await next();
+});
 module.exports = checkRouter;
