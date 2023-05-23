d3d25980ded0b04948b6c7ac584cfb5cac9902d5
by <916306719@qq.com>
2023-05-23 09:45:25 +0800

    feat:独立内容，回收站功能

--- pages/nkc/document/document.js
+++ pages/nkc/document/document.js
@@ -1,47 +1,105 @@
 window.deleteArticle = function (_id) {
-  if(!window.DisabledPost) {
+  if (!window.DisabledPost) {
     window.DisabledPost = new NKC.modules.DisabledPost();
   }
-  window.DisabledPost.open(function(data) {
+  window.DisabledPost.open(function (data) {
     var body = {
-      delType: data.type === 'toDraft'?'faulty':'disabled',
+      delType: data.type === 'toDraft' ? 'faulty' : 'disabled',
       docId: _id,
       type: 'document',
       reason: data.reason,
       remindUser: data.remindUser,
-      violation: data.violation
+      violation: data.violation,
     };
     DisabledPost.lock();
     nkcAPI('/review', 'PUT', body)
-      .then(function() {
-        screenTopAlert("操作成功");
+      .then(function () {
+        screenTopAlert('操作成功');
         DisabledPost.close();
         DisabledPost.unlock();
       })
-      .catch(function(data) {
+      .catch(function (data) {
         sweetError(data);
         DisabledPost.unlock();
-      })
+      });
   });
-}
+};
 
 window.deleteMoment = function (momentId) {
-  if(!momentId) return;
-  sweetQuestion("你确定要删除吗？")
-    .then(() => nkcAPI(`/moment/${momentId}`, "DELETE"))
+  if (!momentId) {
+    return;
+  }
+  sweetQuestion('你确定要删除吗？')
+    .then(() => nkcAPI(`/moment/${momentId}`, 'DELETE'))
     .then(() => {
       sweetSuccess('操作成功');
     })
-    .catch(err => {
+    .catch((err) => {
       sweetError(err);
-    })
-}
+    });
+};
 
-window.recoveryMoment = function(momentId) {
-  if(!momentId) return;
+window.recoveryMoment = function (momentId) {
+  if (!momentId) {
+    return;
+  }
   return nkcAPI(`/moment/${momentId}/recovery`, 'POST')
     .then(() => {
       sweetSuccess('操作成功');
     })
     .catch(sweetError);
-}
+};
+
+window.selectAllContent = function (dom) {
+  document.getElementsByClassName('classification').forEach((checkbox) => {
+    checkbox.checked = dom.checked;
+  });
+};
+
+window.handleSearch = function () {
+  let selectedValues = [];
+  let selectedStatus = [];
+  const t = { source: [], status: [] };
+  document.getElementsByClassName('classification').forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedValues.push(checkbox.value);
+    }
+  });
+  document.getElementsByClassName('status').forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedStatus.push(checkbox.value);
+    }
+  });
+  if (selectedValues.length === 0) {
+    sweetError('请勾选所查询内容');
+  } else if (selectedStatus.length === 0) {
+    sweetError('请勾选所查询内容的状态');
+  } else {
+    t.source = selectedValues;
+    t.status = selectedStatus;
+    const jsonString = encodeURIComponent(JSON.stringify(t));
+    window.location.href = `/nkc/document?a=${jsonString}`;
+  }
+};
+
+window.uncheck = function () {
+  let selectedValues = [];
+  const allContentDom = document.getElementById('all');
+  const allValues = document.getElementsByClassName('classification');
+  allValues.forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedValues.push(checkbox.value);
+    }
+  });
+  if (
+    allContentDom.checked !== true &&
+    selectedValues.length === allValues.length - 1
+  ) {
+    allContentDom.checked = true;
+  } else if (
+    allContentDom.checked === true &&
+    selectedValues.length !== allValues.length
+  ) {
+    allContentDom.checked = false;
+  }
+};
--- routes/nkc/document/index.js
+++ routes/nkc/document/index.js
@@ -1,184 +1,205 @@
 const router = require('koa-router')();
-router
-  .get('/', async (ctx, next) => {
-    const {db, data, query, state, nkcModules} = ctx;
-    let {page = 0, t = ''} = query;
-    const {getUrl} = nkcModules.tools;
+router.get('/', async (ctx, next) => {
+  const { db, data, query, state, nkcModules } = ctx;
+  let { page = 0, t = '', a = '' } = query;
+  const { getUrl } = nkcModules.tools;
+  const { stable: stableDocumentType } =
+    await db.DocumentModel.getDocumentTypes();
+  const documentSourcesObj = await db.DocumentModel.getDocumentSources();
+  const articleSourcesObj = await db.ArticleModel.getArticleSources();
+  const commentSourcesObj = await db.CommentModel.getCommentSources();
+  delete documentSourcesObj.draft;
+  const documentSources = Object.values(documentSourcesObj);
+  const match = {
+    type: stableDocumentType,
+    source: { $ne: documentSourcesObj.draft },
+  };
+  if (t) {
+    if (documentSources.includes(t)) {
+      match.source = t;
+    } else {
+      ctx.throw(400, `document source error. source=${t}`);
+    }
+  }
+  if (a) {
+    const { source, status } = JSON.parse(a);
+  }
+
+  const count = await db.DocumentModel.countDocuments(match);
+  const paging = nkcModules.apiFunction.paging(page, count);
+  const documents = await db.DocumentModel.find(match)
+    .sort({ toc: -1 })
+    .skip(paging.start)
+    .limit(paging.perpage);
+  const usersId = [];
+  const momentsId = [];
+  const articlesId = [];
+  const commentsId = [];
+  const momentsListObj = {};
+  for (const document of documents) {
+    const { uid, source, sid } = document;
+    usersId.push(uid);
+    switch (source) {
+      case documentSourcesObj.article: {
+        articlesId.push(sid);
+        break;
+      }
+      case documentSourcesObj.moment: {
+        momentsId.push(sid);
+        break;
+      }
+      case documentSourcesObj.comment: {
+        commentsId.push(sid);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  const usersObj = await db.UserModel.getUsersObjectByUsersId(usersId);
+  const articlesObj = await db.ArticleModel.getArticlesObjectByArticlesId(
+    articlesId,
+  );
+  const momentsObj = await db.MomentModel.getMomentsObjectByMomentsId(
+    momentsId,
+  );
+  const momentsList = await db.MomentModel.extendMomentsListData(
+    Object.values(momentsObj),
+    state.uid,
+  );
+  const commentsObj = await db.CommentModel.getCommentsObjectByCommentsId(
+    commentsId,
+  );
+  const commentsInfo = await db.CommentModel.getCommentsInfo(
+    Object.values(commentsObj),
+  );
+  const commentsUrl = {};
+  for (const commentInfo of commentsInfo) {
+    commentsUrl[commentInfo._id] = commentInfo.url;
+  }
+  for (const m of momentsList) {
+    momentsListObj[m.momentId] = m;
+  }
+  const documentsInfo = [];
+  for (const document of documents) {
     const {
-      stable: stableDocumentType
-    } = await db.DocumentModel.getDocumentTypes();
-    const documentSourcesObj = await db.DocumentModel.getDocumentSources();
-    const articleSourcesObj = await db.ArticleModel.getArticleSources();
-    const commentSourcesObj = await db.CommentModel.getCommentSources();
-    delete documentSourcesObj.draft;
-    const documentSources = Object.values(documentSourcesObj);
-    const match = {
-      type: stableDocumentType,
-      source: {$ne: documentSourcesObj.draft}
+      _id,
+      did,
+      toc,
+      uid,
+      source,
+      sid,
+      abstract = '',
+      abstractEN = '',
+      content = '',
+      title = '',
+      // keywords = [],
+      // keywordsEN = [],
+      status = '',
+    } = document;
+    const keywords = document.keywords || [];
+    const keywordsEN = document.keywordsEN || [];
+    const user = usersObj[uid];
+
+    let from;
+    let url;
+    let momentInfo;
+
+    switch (source) {
+      case documentSourcesObj.article: {
+        const article = articlesObj[sid];
+        if (!article) {
+          break;
+        }
+        const { articleUrl } = await db.ArticleModel.getArticleUrlBySource(
+          article._id,
+          article.source,
+          article.sid,
+        );
+        url = articleUrl;
+        switch (article.source) {
+          case articleSourcesObj.column: {
+            from = '专栏文章';
+            break;
+          }
+          case articleSourcesObj.zone: {
+            from = '空间文章';
+            break;
+          }
+          default:
+            break;
+        }
+        break;
+      }
+      case documentSourcesObj.moment: {
+        const moment = momentsObj[sid];
+        momentInfo = momentsListObj[moment._id];
+        url = getUrl('zoneMoment', moment._id);
+        from = '动态';
+        break;
+      }
+      case documentSourcesObj.comment: {
+        const comment = commentsObj[sid];
+        url = commentsUrl[comment._id];
+        switch (comment.source) {
+          case commentSourcesObj.zone: {
+            from = '空间文章评论';
+            break;
+          }
+          case commentSourcesObj.column: {
+            from = '专栏文章评论';
+            break;
+          }
+          default:
+            break;
+        }
+        break;
+      }
+    }
+    const documentResourceId = await document.getResourceReferenceId();
+    let resources = await db.ResourceModel.getResourcesByReference(
+      documentResourceId,
+    );
+    const result = {
+      _id,
+      did,
+      toc,
+      // 作者相关
+      uid,
+      username: user.username,
+      userAvatarUrl: getUrl('userAvatar', user.avatar),
+      // 内容相关
+      abstract,
+      abstractEN,
+      keywords: keywords || [],
+      keywordsEN: keywordsEN || [],
+      title,
+      content: nkcModules.nkcRender.renderHTML({
+        type: 'article',
+        post: {
+          c: content,
+          resources,
+        },
+      }),
+      // 类型相关
+      from, // string 来源
+      url,
+      status,
+      source,
     };
-    if(t) {
-      if(documentSources.includes(t)) {
-        match.source = t;
-      } else {
-        ctx.throw(400, `document source error. source=${t}`);
-      }
+    //如果是动态就加入动态资源和引用信息
+    if (source === documentSourcesObj.moment) {
+      result.momentInfo = momentInfo;
     }
-    const count = await db.DocumentModel.countDocuments(match);
-    const paging = nkcModules.apiFunction.paging(page, count);
-    const documents = await db.DocumentModel.find(match).sort({toc: -1}).skip(paging.start).limit(paging.perpage);
-    const usersId = [];
-    const momentsId = [];
-    const articlesId = [];
-    const commentsId = [];
-    const momentsListObj = {};
-    for(const document of documents) {
-      const {
-        uid, source, sid,
-      } = document;
-      usersId.push(uid);
-      switch(source) {
-        case documentSourcesObj.article: {
-          articlesId.push(sid);
-          break;
-        }
-        case documentSourcesObj.moment: {
-          momentsId.push(sid);
-          break;
-        }
-        case documentSourcesObj.comment: {
-          commentsId.push(sid);
-          break;
-        }
-        default: break;
-      }
-    }
+    documentsInfo.push(result);
+  }
 
-    const usersObj = await db.UserModel.getUsersObjectByUsersId(usersId);
-    const articlesObj = await db.ArticleModel.getArticlesObjectByArticlesId(articlesId);
-    const momentsObj = await db.MomentModel.getMomentsObjectByMomentsId(momentsId);
-    const momentsList = await db.MomentModel.extendMomentsListData(Object.values(momentsObj), state.uid);
-    const commentsObj = await db.CommentModel.getCommentsObjectByCommentsId(commentsId);
-    const commentsInfo = await db.CommentModel.getCommentsInfo(Object.values(commentsObj));
-    const commentsUrl = {};
-    for(const commentInfo of commentsInfo) {
-      commentsUrl[commentInfo._id] = commentInfo.url;
-    }
-    for(const m of momentsList) {
-      momentsListObj[m.momentId] = m;
-    }
-    const documentsInfo = [];
-    for(const document of documents) {
-      const {
-        _id,
-        did,
-        toc,
-        uid,
-        source,
-        sid,
-        abstract = '',
-        abstractEN = '',
-        content = '',
-        title = '',
-        // keywords = [],
-        // keywordsEN = [],
-        status = ''
-      } = document;
-      const keywords = document.keywords || [];
-      const keywordsEN = document.keywordsEN || [];
-      const user = usersObj[uid];
-
-      let from;
-      let url;
-      let momentInfo;
-
-      switch(source) {
-        case documentSourcesObj.article: {
-          const article = articlesObj[sid];
-          if(!article) break;
-          const {articleUrl} = await db.ArticleModel.getArticleUrlBySource(
-            article._id,
-            article.source,
-            article.sid,
-          );
-          url = articleUrl;
-          switch(article.source) {
-            case articleSourcesObj.column: {
-              from = '专栏文章';
-              break;
-            }
-            case articleSourcesObj.zone: {
-              from = '空间文章';
-              break;
-            }
-            default: break;
-          }
-          break;
-        }
-        case documentSourcesObj.moment: {
-          const moment = momentsObj[sid];
-          momentInfo = momentsListObj[moment._id];
-          url = getUrl('zoneMoment', moment._id);
-          from = '动态';
-          break;
-        }
-        case documentSourcesObj.comment: {
-          const comment = commentsObj[sid];
-          url = commentsUrl[comment._id];
-          switch(comment.source) {
-            case commentSourcesObj.zone: {
-              from = '空间文章评论';
-              break;
-            }
-            case commentSourcesObj.column: {
-              from = '专栏文章评论';
-              break;
-            }
-            default: break;
-          }
-          break;
-        }
-      }
-      const documentResourceId = await document.getResourceReferenceId();
-      let resources = await db.ResourceModel.getResourcesByReference(documentResourceId);
-      const result = {
-        _id,
-        did,
-        toc,
-        // 作者相关
-        uid,
-        username: user.username,
-        userAvatarUrl: getUrl('userAvatar', user.avatar),
-        // 内容相关
-        abstract,
-        abstractEN,
-        keywords: keywords || [],
-        keywordsEN: keywordsEN || [],
-        title,
-        content: nkcModules.nkcRender.renderHTML({
-          type: 'article',
-          post: {
-            c: content,
-            resources
-          },
-        }),
-        // 类型相关
-        from, // string 来源
-        url,
-        status,
-        source,
-      };
-      //如果是动态就加入动态资源和引用信息
-      if(source === documentSourcesObj.moment) {
-        result.momentInfo = momentInfo;
-      }
-      documentsInfo.push(result);
-    }
-
-    data.documentsInfo = documentsInfo;
-    data.t = t;
-    data.paging = paging;
-    ctx.template = 'nkc/document/document.pug';
-    data.nav = 'document';
-    await next();
-  });
+  data.documentsInfo = documentsInfo;
+  data.t = t;
+  data.paging = paging;
+  ctx.template = 'nkc/document/document.pug';
+  data.nav = 'document';
+  await next();
+});
 module.exports = router;
