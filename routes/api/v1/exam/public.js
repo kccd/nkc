423716af109d8b01b2bc68221e6225f5fd70fabb
by <916306719@qq.com>
2023-08-03 18:13:29 +0800

    feat:优化

--- routes/api/v1/exam/public.js
+++ routes/api/v1/exam/public.js
@@ -12,7 +12,7 @@ router
     ctx.apiData = { publicExamNotes };
     await next();
   })
-  .get('/register', OnlyVisitor(), async (ctx, next) => {
+  .get('/register', async (ctx, next) => {
     const { db } = ctx;
     const {
       c: { registerExamination },
@@ -22,30 +22,22 @@ router
     };
     await next();
   })
-  .get(
-    '/paper',
-    OnlyPermission(FixedOperations.visitPublicExam),
-    async (ctx, next) => {
-      const { db } = ctx;
-      const {
-        c: { examSource },
-      } = await db.SettingModel.findOnly({ _id: 'register' }, { c: 1 });
-      ctx.apiData = {
-        examSource,
-      };
-      await next();
-    },
-  )
-  .post(
-    '/result',
-    OnlyPermission(FixedOperations.visitPublicExam),
-    async (ctx, next) => {
-      const randomize1 = randomize('A0', 64);
-      ctx.apiData = {
-        success: '成功了',
-      };
-      await next();
-    },
-  );
+  .get('/paper', async (ctx, next) => {
+    const { db } = ctx;
+    const {
+      c: { examSource },
+    } = await db.SettingModel.findOnly({ _id: 'register' }, { c: 1 });
+    ctx.apiData = {
+      examSource,
+    };
+    await next();
+  })
+  .post('/result', async (ctx, next) => {
+    const randomize1 = randomize('A0', 64);
+    ctx.apiData = {
+      success: '成功了',
+    };
+    await next();
+  });
 
 module.exports = router;
