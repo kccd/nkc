1fe544a89bdb3577e1ffb4b897cfd33a9f6255af
pxgo <pengxiguaa@gmail.com>
2023-08-07 11:30:52 +0800

    feat: 基金审核评语表单添加浮窗模式

--- pages/fund/manage/audit/audit.js
+++ pages/fund/manage/audit/audit.js
@@ -1,8 +1,17 @@
-const appElement = document.getElementById('app');
-const {applicationForm, type} = NKC.methods.getDataById('data');
-applicationForm.budgetMoney.map(b => {
-  if(b.suggest === null) b.suggest = b.total;
-  if(b.fact === null) b.fact = b.suggest;
+import { getDataById } from '../../../lib/js/dataConversion';
+import Vue from 'vue';
+import { sweetPrompt, sweetError } from '../../../lib/js/sweetAlert';
+import { visitUrl } from '../../../lib/js/pageSwitch';
+import { nkcAPI } from '../../../lib/js/netAPI';
+import CommonModal from '../../../lib/vue/CommonModal.vue';
+const { applicationForm, type } = getDataById('data');
+applicationForm.budgetMoney.map((b) => {
+  if (b.suggest === null) {
+    b.suggest = b.total;
+  }
+  if (b.fact === null) {
+    b.fact = b.suggest;
+  }
 });
 window.app = new Vue({
   el: '#app',
@@ -11,77 +20,80 @@ window.app = new Vue({
     applicationForm,
     budgetMoney: applicationForm.budgetMoney,
     times: 2,
-    refuseReason: ''
+    refuseReason: '',
+  },
+  components: {
+    'common-modal': CommonModal,
   },
   mounted() {
-    if(this.type === 'info') {
+    if (this.type === 'info') {
       this.setRemittance();
     }
   },
   computed: {
     total() {
       let num = 0;
-      const {budgetMoney} = this;
-      for(const b of budgetMoney) {
+      const { budgetMoney } = this;
+      for (const b of budgetMoney) {
         num += b.count * (b.money * 100);
       }
       return num / 100;
     },
     suggestTotal() {
       let num = 0;
-      const {budgetMoney} = this;
-      for(const b of budgetMoney) {
+      const { budgetMoney } = this;
+      for (const b of budgetMoney) {
         num += b.suggest * 100;
       }
       return num / 100;
     },
     factTotal() {
       let num = 0;
-      const {budgetMoney} = this;
-      for(const b of budgetMoney) {
+      const { budgetMoney } = this;
+      for (const b of budgetMoney) {
         num += b.fact * 100;
       }
       return num / 100;
     },
     factMoney() {
-      const {applicationForm, factTotal} = this;
-      if(applicationForm.fixedMoney) {
+      const { applicationForm, factTotal } = this;
+      if (applicationForm.fixedMoney) {
         return applicationForm.money;
       } else {
         return factTotal;
       }
     },
     fixedMoneyRadio() {
-      const {type, total, suggestTotal, factTotal} = this;
+      const { type, total, suggestTotal, factTotal } = this;
       let newTotal, name;
-      if(type === 'project') {
+      if (type === 'project') {
         newTotal = suggestTotal;
         name = '专家建议';
       } else {
         newTotal = factTotal;
         name = '实际批准';
       }
-      if(newTotal / total < 0.8) {
+      if (newTotal / total < 0.8) {
         return name + '金额小于预算金额的 80%，资金预算审核只能选择不合格';
       }
     },
     remittanceInfo() {
-      const {remittance} = this.applicationForm;
+      const { remittance } = this.applicationForm;
       let total = 0;
       for (const r of remittance) {
-        if(r.money < 0.01) {
+        if (r.money < 0.01) {
           return `每期金额不能小于 0.01 元`;
         }
         total += r.money * 100;
       }
-      if(total !== this.factMoney * 100) {
-        return `分期金额不等于实际批准金额`
+      if (total !== this.factMoney * 100) {
+        return `分期金额不等于实际批准金额`;
       }
-    }
+    },
   },
   methods: {
     setRemittance() {
-      let {factMoney, times} = this;
+      let { factMoney, times } = this;
       factMoney *= 100;
       const money = Math.floor(factMoney / times);
       const remainder = factMoney - money * times;
@@ -89,12 +101,74 @@ window.app = new Vue({
       arr.fill(money / 100);
       arr.push((money + remainder) / 100);
       const remittance = [];
-      for(const m of arr) {
+      for (const m of arr) {
         remittance.push({
           money: m,
         });
       }
       this.applicationForm.remittance = remittance;
+    },
+    getReasonInfo(type) {
+      const reason = $(`div[data-type="${type}"] textarea`).val();
+      const passed = $(`div[data-type="${type}"] input`).eq(0).prop('checked');
+      return {
+        reason,
+        passed,
+      };
+    },
+    setReasonInfo(type, props) {
+      $(`div[data-type="${type}"] textarea`).val(props.reason);
+      const targetInput = $(`div[data-type="${type}"] input`);
+      targetInput.eq(0).prop('checked', props.passed);
+      targetInput.eq(1).prop('checked', !props.passed);
+    },
+    getTitleByType(type) {
+      let title = '';
+      if (type === 'userInfoAudit') {
+        title = '审核人信息';
+      } else if (type === 'projectAudit') {
+        title = '项目信息';
+      } else {
+        title = '资金预算';
+      }
+      return title;
+    },
+    openReasonInput(type) {
+      let title = this.getTitleByType(type);
+      const reasonObj = this.getReasonInfo(type);
+      this.$refs.commonModal.open(
+        (data) => {
+          this.setReasonInfo(type, {
+            reason: data[0].value,
+            passed: data[1].value,
+          });
+          this.$refs.commonModal.close();
+        },
+        {
+          title: `审核评语 - ${title}`,
+          data: [
+            {
+              dom: 'textarea',
+              type: 'text',
+              value: reasonObj.reason,
+            },
+            {
+              dom: 'radio',
+              value: reasonObj.passed,
+              radios: [
+                {
+                  value: true,
+                  name: '合格',
+                },
+                {
+                  value: false,
+                  name: '不合格',
+                },
+              ],
+            },
+          ],
+        },
+      );
     },
     formatMoney(index) {
       const m = this.budgetMoney[index];
@@ -107,98 +181,108 @@ window.app = new Vue({
       return sweetPrompt(`请输入拒绝的理由`, this.refuseReason)
         .then((reason) => {
           self.refuseReason = reason;
-          return nkcAPI(`/fund/a/${self.applicationForm._id}/manage/refuse`, 'POST', {
-            reason
-          })
+          return nkcAPI(
+            `/fund/a/${self.applicationForm._id}/manage/refuse`,
+            'POST',
+            {
+              reason,
+            },
+          );
         })
         .then(() => {
-          NKC.methods.visitUrl(`/fund/a/${self.applicationForm._id}`);
+          visitUrl(`/fund/a/${self.applicationForm._id}`);
         })
-        .catch(err => {
+        .catch((err) => {
           sweetError(err);
-        })
+        });
     },
     submit() {
       const self = this;
       return Promise.resolve()
         .then(() => {
-          if(self.type === 'project') {
+          if (self.type === 'project') {
             const userInfoReason = getReasonObj('userInfoAudit');
             const projectReason = getReasonObj('projectAudit');
             const moneyReason = getReasonObj('moneyAudit');
 
-            if(self.fixedMoneyRadio && moneyReason.passed) {
+            if (self.fixedMoneyRadio && moneyReason.passed) {
               throw new Error(self.fixedMoneyRadio);
             }
-            const suggest = self.budgetMoney.map(b => b.suggest);
+            const suggest = self.budgetMoney.map((b) => b.suggest);
 
             const comments = [
               {
                 type: 'userInfoAudit',
                 c: userInfoReason.reason,
-                support: userInfoReason.passed
+                support: userInfoReason.passed,
               },
               {
                 type: 'projectAudit',
                 c: projectReason.reason,
-                support: projectReason.passed
+                support: projectReason.passed,
               },
               {
                 type: 'moneyAudit',
                 c: moneyReason.reason,
-                support: moneyReason.passed
-              }
+                support: moneyReason.passed,
+              },
             ];
-            return nkcAPI(`/fund/a/${self.applicationForm._id}/manage/audit/project`, 'POST', {
-              comments,
-              suggest
-            })
+            return nkcAPI(
+              `/fund/a/${self.applicationForm._id}/manage/audit/project`,
+              'POST',
+              {
+                comments,
+                suggest,
+              },
+            );
           } else {
             const moneyReason = getReasonObj('adminAudit');
-            if(self.fixedMoneyRadio && moneyReason.passed) {
+            if (self.fixedMoneyRadio && moneyReason.passed) {
               throw new Error(self.fixedMoneyRadio);
             }
-            const fact = self.budgetMoney.map(b => b.fact);
+            const fact = self.budgetMoney.map((b) => b.fact);
             const comment = {
               type: 'adminAudit',
               c: moneyReason.reason,
               support: moneyReason.passed,
             };
-            return nkcAPI(`/fund/a/${self.applicationForm._id}/manage/audit/info`, 'POST', {
-              comment,
-              fact,
-              reportNeedThreads: applicationForm.reportNeedThreads,
-              remittance: applicationForm.remittance.map(r => r.money),
-            });
+            return nkcAPI(
+              `/fund/a/${self.applicationForm._id}/manage/audit/info`,
+              'POST',
+              {
+                comment,
+                fact,
+                reportNeedThreads: applicationForm.reportNeedThreads,
+                remittance: applicationForm.remittance.map((r) => r.money),
+              },
+            );
           }
-
         })
         .then(() => {
-          NKC.methods.visitUrl(`/fund/a/${self.applicationForm._id}`);
+          visitUrl(`/fund/a/${self.applicationForm._id}`);
         })
-        .catch(err => {
+        .catch((err) => {
           sweetError(err);
         });
-
-    }
-  }
-})
+    },
+  },
+});
 
 const types = {
-  'moneyAudit': '资金预算',
-  'userInfoAudit': '申请人信息',
-  'projectAudit': '项目信息',
-  'adminAudit': ''
+  moneyAudit: '资金预算',
+  userInfoAudit: '申请人信息',
+  projectAudit: '项目信息',
+  adminAudit: '',
 };
 
 function getReasonObj(type) {
   const reason = $(`div[data-type="${type}"] textarea`).val();
   const passed = $(`div[data-type="${type}"] input`).eq(0).prop('checked');
-  if(!passed && reason.length === 0) {
+  if (!passed && reason.length === 0) {
     throw new Error(`${types[type]}审核评语不能为空`);
   }
   return {
     reason,
-    passed
-  }
+    passed,
+  };
 }
