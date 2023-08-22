8f2d7e26555247c2c6d8cb2093ff969cdd86f9c4
PengXigui <pengxiguaa@gmail.com>
2023-08-22 14:34:38 +0800

    优化：优化考试注册逻辑

--- routes/api/v1/exam/public.js
+++ routes/api/v1/exam/public.js
@@ -150,9 +150,10 @@ router
           score,
         },
       );
-      // const { _id } = await paperService.createActivationCodeByPaperId(pid);
+      const activationCode = await paperService.createActivationCodeByPaperId(pid);
       ctx.apiData = {
-        src: `/login?t=register`,
+        redirectUrl: `/login?t=register`,
+        activationCode: activationCode._id,
       };
     }
     await next();
