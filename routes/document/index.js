ffd121c57bb21c7024888926d7c07eba42bdda9b
PengXigui <pengxiguaa@gmail.com>
2023-09-04 16:53:19 +0800

    fix: 修复专栏独立文章历史页报错问题

--- routes/document/index.js
+++ routes/document/index.js
@@ -1,51 +1,42 @@
 const router = require('koa-router')();
 const nkcRender = require('../../nkcModules/nkcRender');
-/*
- * 如果是专栏文章预览就返回分类信息
- */
-// async function setCategory(data, state, db, source) {
-//   if(source === 'article'){
-//     const articleSource = await db.ArticleModel.findOne({_id: data.document.sid, uid: state.uid},{source: 1});
-//     if(articleSource.source === "column"){
-//       const category = await db.ColumnPostModel.findOne({pid: articleSource._id, tUid: state.uid}, { cid: 1, mcid: 1, columnId: 1 })
-//       const { cid, mcid, columnId } = category;
-//       const column = await db.ColumnModel.findOne({_id: columnId}, {name: 1})
-//       // 分类和专栏名
-//       data.mainCategory = await db.ColumnPostCategoryModel.getParentCategoryByIds(cid);
-//       data.auxiliaryCategory = await db.ColumnPostCategoryModel.getArticleAllMinorCategories(mcid);
-//       data.columnName = column.name;
-//     }
-//   }
-// }
+const { DynamicOperations } = require('../../settings/operations');
+
 router
+  .use('/', async (ctx, next) => {
+    const { db, query, state } = ctx;
+    const { source, sid } = query;
+    const { uid: targetUserId } = await db.DocumentModel.findOne(
+      { source, sid },
+      { uid: 1 },
+    );
+    if (
+      state.uid !== targetUserId &&
+      !ctx.permission(DynamicOperations.viewUserArticle)
+    ) {
+      ctx.throw('权限不足');
+    }
+    await next();
+  })
   .get('/preview', async (ctx, next) => {
     //获取文档预览信息
-    ctx.template = 'document/preview/document.pug';
-    const { db, data, state, query, permission, nkcModules } = ctx;
+    const { db, data, query, nkcModules } = ctx;
     const { sid, source } = query;
+    const documentTypes = await db.DocumentModel.getDocumentTypes();
     // 需要返回 分类信息
-    const document = await db.DocumentModel.find({
+    const document = await db.DocumentModel.findOne({
       sid,
       source,
-      uid: state.uid,
-      type: 'beta',
-    })
-      .sort({ tlm: -1 })
-      .skip(0)
-      .limit(1);
-    if (!document.length) {
-      ctx.throw(400, '该文章已被发布');
+      type: documentTypes.beta,
+    }).sort({ tlm: -1 });
+    if (!document) {
+      ctx.throw(400, '没有可以预览的内容');
     }
 
     // 用于pug渲染判断当前是什么类型页面
     data.type = source;
-    data.document = document[0];
+    data.document = document;
     // 访问的文章是否是作者本人如果不是那么判断是否有权限访问
-    if (data.document.uid !== state.uid) {
-      if (!permission('viewUserArticle')) {
-        ctx.throw(403, '没有权限');
-      }
-    }
     const documentResourceId = await data.document.getResourceReferenceId();
     let resources = await db.ResourceModel.getResourcesByReference(
       documentResourceId,
@@ -69,22 +60,19 @@ router
     } else {
       data.document = data.document.toObject();
     }
+    ctx.template = 'document/preview/document.pug';
     await next();
   })
   .get('/history', async (ctx, next) => {
     //获取文档历史版本
     ctx.template = 'document/history/document.pug';
-    const { db, data, state, query, permission, nkcModules } = ctx;
+    const { db, data, state, query, nkcModules } = ctx;
     const { sid, source, page = 0 } = query;
     data.type = source;
     const { betaHistory, stableHistory } =
       await db.DocumentModel.getDocumentTypes();
     const queryCriteria = {
-      $and: [
-        { sid, source },
-        { type: { $in: [betaHistory, stableHistory] } },
-        { uid: state.uid },
-      ],
+      $and: [{ sid, source }, { type: { $in: [betaHistory, stableHistory] } }],
     };
     //  获取列表
     // 返回分页信息
@@ -98,11 +86,6 @@ router
     if (data.history.length) {
       // 默认返回第一项内容
       data.document = data.history[0];
-      if (data.document.uid !== state.uid) {
-        if (!permission('viewUserArticle')) {
-          ctx.throw(403, '没有权限');
-        }
-      }
       // data.document.user = await db.UserModel.findOnly({uid: data.document.uid});
       const documentResourceId = await data.document.getResourceReferenceId();
       let resources = await db.ResourceModel.getResourcesByReference(
@@ -120,7 +103,6 @@ router
       if (source === 'article') {
         article = await db.ArticleModel.findOne({
           _id: data.document.sid,
-          uid: state.uid,
         });
         //  选择此版本进行编辑的url
         editorUrl = await db.ArticleModel.getArticleUrlBySource(
@@ -148,27 +130,27 @@ router
         editorUrl,
         page,
       };
+      const user = await db.UserModel.findOnly({ uid: data.document.uid });
+      const avatarUrl = nkcModules.tools.getUrl('userAvatar', user.avatar);
+      data.user = { ...user.toObject(), avatarUrl };
+      if (data.document.origin !== 0) {
+        const originDesc = await nkcModules.apiFunction.getOriginLevel(
+          data.document.origin,
+        );
+        data.document = { ...data.document.toObject(), originDesc };
+      } else {
+        data.document = data.document.toObject();
+      }
     } else {
       data.document = '';
       // data.bookId = ''
       data.urlComponent = '';
     }
-    const user = await db.UserModel.findOnly({ uid: data.document.uid });
-    const avatarUrl = nkcModules.tools.getUrl('userAvatar', user.avatar);
-    data.user = { ...user.toObject(), avatarUrl };
-    if (data.document.origin !== 0) {
-      const originDesc = await nkcModules.apiFunction.getOriginLevel(
-        data.document.origin,
-      );
-      data.document = { ...data.document.toObject(), originDesc };
-    } else {
-      data.document = data.document.toObject();
-    }
     await next();
   })
   .get('/history/:_id', async (ctx, next) => {
     ctx.template = 'document/history/document.pug';
-    const { db, data, params, state, query, permission, nkcModules } = ctx;
+    const { db, data, params, query, nkcModules } = ctx;
     const { sid, source, page = 0 } = query;
     const { _id } = params;
     data.type = source;
@@ -178,7 +160,6 @@ router
       sid,
       source,
       type: { $in: [betaHistory, stableHistory] },
-      uid: state.uid,
     };
     const count = await db.DocumentModel.countDocuments(queryCriteria);
     const paging = nkcModules.apiFunction.paging(page, count, 10);
@@ -200,11 +181,6 @@ router
       // 如果添加了很多历史记录，而没有刷新，直接点击历史就可能出现在当前页找不到指定的数据，因为数据发生了改变（主要是可能排在了其他页中）
       data.document = data.history[0];
     }
-    if (data.document.uid !== state.uid) {
-      if (!permission('viewUserArticle')) {
-        ctx.throw(403, '没有权限');
-      }
-    }
     data.paging = paging;
     const documentResourceId = await data.document.getResourceReferenceId();
     let resources = await db.ResourceModel.getResourcesByReference(
@@ -222,7 +198,6 @@ router
     if (source === 'article') {
       article = await db.ArticleModel.findOne({
         _id: data.document.sid,
-        uid: state.uid,
       });
       //  选择此版本进行编辑的url
       editorUrl = await db.ArticleModel.getArticleUrlBySource(
