a8e6e3668c85ddb776a98e1ea9ab7b7bcf31552d
by <916306719@qq.com>
2023-09-06 17:15:35 +0800

    feat:前端样式优化

--- pages/lib/vue/PublicPaper.vue
+++ pages/lib/vue/PublicPaper.vue
@@ -26,27 +26,31 @@
               span(v-if="answerDesc.desc") {{answerDesc.desc}}
           form(v-else)
             label.options(v-if="question.isMultiple" v-for='(q, index) in question.answer' :class="bgc(index,q)")
-              input.m-r-05(:disabled="isReselected" v-show="false" type="checkbox" :name="'question' + index" :value='index' v-model='selected' )
+              input.m-r-05(:disabled="isReselected || isCorrect " v-show="false" type="checkbox" :name="'question' + index" :value='index' v-model='selected' )
               .option-content
                 span {{q.serialIndex}}、
                 span.m-r-2 {{q.text}}
-                .answer-desc.text-danger(v-if="answerDescObj[q._id] && answerDescObj[q._id].desc.length > 0 && selected.includes(index)")
+                .answer-desc.text-danger(v-if="answerDescObj[q._id] && answerDescObj[q._id].desc.length > 0")
                   .answer-desc-icon.fa.fa-lightbulb-o
                   span {{answerDescObj[q._id].desc}}
             label.options(v-if="!question.isMultiple" v-for='(q, index) in question.answer' :class="bgc(index,q)")
-              input.m-r-05(:disabled="isReselected" v-show="false" type="radio" :name="'question' + index" :value='index' v-model='selected' )
+              input.m-r-05(:disabled="isReselected || isCorrect " v-show="false" type="radio" :name="'question' + index" :value='index' v-model='selected' )
               .option-content
                 span {{q.serialIndex}}、
                 span.m-r-2 {{q.text}}
-                .answer-desc.text-danger(v-if="answerDescObj[q._id] && answerDescObj[q._id].desc.length > 0 && selected === index")
+                .answer-desc.text-danger(v-if="answerDescObj[q._id] && answerDescObj[q._id].desc.length > 0 ")
                   .answer-desc-icon.fa.fa-lightbulb-o
                   span {{answerDescObj[q._id].desc}}
         footer.clearfix
-          h4.question-intro.text-danger(v-if="!isCorrect" ) 回答错误
+          h4.question-intro.text-danger(v-if="isCorrect === false" ) 回答错误
+          h4.question-intro.text-success(v-if="isCorrect === true" ) 回答正确
           .button-group
             button.btn.btn-default.btn-editor-block.m-r-1(v-if="question.contentDesc" @click="showReminder") 查看提示
             button.btn.btn-default.btn-editor-block.btn-info(v-if="isReselected && type === 'ch4'" @click='reselected') 重选
-            button.btn.btn-default.btn-editor-block.btn-primary(v-else @click='submit' :disabled="isDisabled" :title="isDisabled?'答案不能为空':''") 提交
+            button.btn.btn-default.btn-editor-block.btn-primary(@click='pre' :disabled="!(index - 1 >= 0)" ) 上一题
+            button.btn.btn-default.btn-editor-block.btn-primary(@click='next' :disabled="isCorrect !== true" ) 下一题
+            button.btn.btn-default.btn-editor-block.btn-primary(v-if="!isReselected && isCorrect !== true" @click='submit' :disabled="isDisabled" :title="isDisabled?'答案不能为空':''") 提交
+
     div.clearfix(v-else id="finished-box" )
       .notice-content
         .glyphicon.glyphicon-ok.icon-success.m-b-1
@@ -266,7 +270,7 @@ export default Vue.extend({
       selected: [], //当前用户选择题所选
       fill: '', //当前填空题用户所填
       answerDesc: [], //题目的简介，每个选项的简介
-      isReselected:false, //是否重置
+      isReselected: false, //是否重置
       answerOrder:[], //存储选择题的题目顺序
       type:'',
       isFinished:false,//用户是否做完
@@ -275,7 +279,7 @@ export default Vue.extend({
       paperTime: '',
       paperQuestionCount: "",
       paperName: '',
-      isCorrect:true
+      isCorrect:null,
     };
   },
   props:['pid'],
@@ -312,13 +316,13 @@ export default Vue.extend({
     getUrl,
     detailedTime,
     //获取考题
-    getInit() {
-      nkcAPI(`/api/v1/exam/public/paper/${this.pid}?index=${this.index}`, 'GET')
+    getInit(type='default',index = 0) {
+      nkcAPI(`/api/v1/exam/public/paper/${this.pid}?index=${index}&&type=${type}`, 'GET')
         .then((res) => {
           if (res) {
             const { question, questionTotal, index, paperName, paperTime, paperQuestionCount } =
               res.data;
-            const { answer, type, ...params } = question;
+            const {correct ,answer, type, ...params } = question;
             const oldAnswer = JSON.parse(JSON.stringify(answer));
             //选择题
             if (type === 'ch4') {
@@ -334,13 +338,34 @@ export default Vue.extend({
             } else {
               this.question = question;
             }
+            //已经做过了,保留之前的状态
+            if(question.correct){
+              this.isCorrect = true
+              //多选
+              if(type === 'ch4'){
+                if(this.question.isMultiple){
+                  this.selected = question.answer
+                    .map((item, index) => (item.selected ? index : undefined))
+                    .filter(index => index !== undefined);
+                }else {
+                  this.selected = question.answer.findIndex(item => item.selected)
+                }
+                this.answerDesc =  this.question.answer.map((item) => {
+                  const { desc, _id } = item;
+                  return { desc, _id };
+                });
+              }
+              else {
+                this.answerDesc.desc = question.answer[0].desc
+                this.fill = question.answer[0].fill
+              }
+            }
             this.questionTotal = questionTotal;
-            this.index = index;
+            this.index = Number(index) ;
             this.type = type;
             this.paperName = paperName;
             this.paperTime = paperTime;
             this.paperQuestionCount = paperQuestionCount;
-
             setTimeout(() => {
               renderFormula()
             }, 500)
@@ -350,6 +375,7 @@ export default Vue.extend({
           sweetError(error);
         });
     },
+    //提交
     submit() {
       const {isMultiple} = this.question
       let userSelected = [];
@@ -361,7 +387,6 @@ export default Vue.extend({
           }
           return selectedIds;
         }, []);
-
         selected = this.answerOrder.reduce((result, item, index) => {
           if (userSelected.includes(item)) {
             result.push(index);
@@ -378,7 +403,7 @@ export default Vue.extend({
       })
         .then((res) => {
           if (res.data) {
-            const { status, answerDesc, index } = res.data;
+            const { status, answerDesc} = res.data;
             if (status === 403) {
               if (this.type === 'ch4'){
                 this.isReselected = true;
@@ -389,19 +414,14 @@ export default Vue.extend({
               }
               this.answerDesc = answerDesc;
             } else if (status === 200) {
-              this.answerDesc = [];
-              this.selected = [];
-              this.fill = '';
-              if (index <= this.questionTotal - 1) {
-                this.index = index;
-                this.getInit();
-              } else {
+              this.isCorrect = true;
+              this.answerDesc = answerDesc;
+              if (this.index === this.questionTotal - 1) {
                 nkcAPI(
                   `/api/v1/exam/public/final-result/${this.pid}`,
                   'POST',
                 ).then((res) => {
                   if (res) {
-                    // sweetSuccess('顺利完成');
                     const { activationCode, redirectUrl,from} = res.data;
                     if (activationCode) {
                       setRegisterActivationCodeToLocalstorage(activationCode);
@@ -422,6 +442,7 @@ export default Vue.extend({
     showReminder(){
       sweetInfo(this.question.contentDesc)
     },
+    //重置
     reselected(){
       if(this.type === 'ch4'){
         this.selected = [];
@@ -429,8 +450,9 @@ export default Vue.extend({
       }
       this.answerDesc = [];
       this.isReselected = false;
-      this.isCorrect = true;
+      this.isCorrect = null;
     },
+    //排序
     shuffle(arr){
       const length = arr.length;
       for(let i = 0; i < length; i++) {
@@ -445,21 +467,46 @@ export default Vue.extend({
         a.serialIndex = alphabetArray[index];
       });
     },
+    //背景颜色
     bgc(index,q){
+      //多选
       const isMultiple = this.question.isMultiple;
+      //是否选择
       const isInclude = isMultiple ? this.selected.includes(index) : this.selected === index;
-      const isFalse = this.answerDesc.length > 0;
+      const {isCorrect} = this
       if (!isInclude) {
         return 'bg-secondary';
+      } else if (isCorrect === null) {
+        return 'bg-info'
+      } else if (isCorrect === true) {
+        return 'bg-success';
+      } else {
+        return  'answer-bg-danger shake-animation';
       }
-      if (!isFalse) {
-        return 'bg-info';
-      }
-      const hasDesc = this.answerDesc.some(item => item._id === q._id && item.desc);
-      return hasDesc ? 'answer-bg-danger' : 'answer-bg-danger shake-animation';
     },
     redirectToPage() {
       visitUrl(this.redirectUrl)
+    },
+    //切换下一题
+    next(){
+      this.answerDesc = [];
+      this.selected = [];
+      this.fill = '';
+      this.isCorrect = null;
+      if(this.index <= this.questionTotal - 1){
+        this.index +=1
+      }
+      this.getInit('down',this.index);
+    },
+    pre(){
+      if(this.index -1 <0){
+        return sweetError('没有上一题')
+      }
+      this.answerDesc = [];
+      this.selected = [];
+      this.fill = '';
+      this.isCorrect = null;
+      this.getInit('up',this.index -1);
     }
   },
 })
