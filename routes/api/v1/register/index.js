99da2c3818da3d093ec19de9a9e8542b26a5fe4b
PengXigui <pengxiguaa@gmail.com>
2023-09-07 12:00:53 +0800

    pref: 优化注册时的短信验证

--- routes/api/v1/register/index.js
+++ routes/api/v1/register/index.js
@@ -1,36 +1,23 @@
 const Router = require('koa-router');
 const {
-  activationCodeService,
-} = require('../../../../services/activationCode/activationCode.service');
-const {
   registerExamService,
 } = require('../../../../services/register/registerExam.service');
 const { getRegisterExamCheckLock } = require('../../../../nkcModules/redLock');
 
 const router = new Router();
 router.get('/exam', async (ctx, next) => {
-  const { query, db, address } = ctx;
+  const { query, address } = ctx;
   const lock = await getRegisterExamCheckLock(address);
   try {
-    const { code: registerActivationCode } = query;
-    const { registerExamination } = await db.SettingModel.getSettings(
-      'register',
-    );
-    let isExamRequired = false;
-    if (registerExamination) {
-      isExamRequired = true;
-      if (registerActivationCode) {
-        await registerExamService.accessRateLimit(address);
-        const valid = await activationCodeService.isActivationCodeValid(
-          registerActivationCode,
-        );
-        if (valid) {
-          isExamRequired = false;
-        }
-      }
+    const { isExamRequired, isExamEnabled, isValidCode } =
+      await registerExamService.getRegisterCodeStatus(query.code);
+    if (isExamEnabled) {
+      await registerExamService.accessRateLimit(address);
     }
     ctx.apiData = {
       isExamRequired,
+      isExamEnabled,
+      isValidCode,
     };
     await lock.unlock();
   } catch (err) {
