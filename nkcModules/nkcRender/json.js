2b698d2c7864f79d1eef02931cbbdcd7b7f1f575
Blucepu <2287892008@qq.com>
2024-10-24 16:26:59 +0800

    “refactor:调整自定义节点渲染”

--- nkcModules/nkcRender/json.js
+++ nkcModules/nkcRender/json.js
@@ -2,13 +2,16 @@ const pug = require('pug');
 const path = require('path');
 const tools = require('../tools');
 const homePugFilePath = path.resolve(__dirname, './nodes/home.pug');
+const videoSize = require('../../settings/video');
 
-function renderHTMLByJSON(json) {
+function renderHTMLByJSON(json, resourcesObj = {}) {
   const jsonObj = typeof json === 'string' ? JSON.parse(json) : json;
   const html = pug.renderFile(homePugFilePath, {
     pretty: true,
     data: jsonObj,
     tools: tools,
+    resourcesObj,
+    videoQuality: videoSize,
     cache: false,
   });
 
