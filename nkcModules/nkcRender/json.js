5a1d13e95ec0c5939890fe1938ffa511f10c0c3f
Blucepu <2287892008@qq.com>
2024-10-24 10:52:01 +0800

    pref:优化测试页面模拟渲染数据

--- nkcModules/nkcRender/json.js
+++ nkcModules/nkcRender/json.js
@@ -1,12 +1,14 @@
 const pug = require('pug');
 const path = require('path');
+const tools = require('../tools');
 const homePugFilePath = path.resolve(__dirname, './nodes/home.pug');
 
 function renderHTMLByJSON(json) {
-  const jsonObj = JSON.parse(json);
+  const jsonObj = typeof json === 'string' ? JSON.parse(json) : json;
   const html = pug.renderFile(homePugFilePath, {
     pretty: true,
     data: jsonObj,
+    tools: tools,
     cache: false,
   });
 
