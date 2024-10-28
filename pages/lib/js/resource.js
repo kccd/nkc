38fc71e195b6899427f55f1f5edcaa809c586e39
Blucepu <2287892008@qq.com>
2024-10-28 18:24:52 +0800

    refactor:编辑器粘贴内容阻止默认图片插入、兼容本站链接图片、添加行内图片渲染节点

--- pages/lib/js/resource.js
+++ pages/lib/js/resource.js
@@ -21,3 +21,26 @@ export function UploadResource(props) {
       }
     });
 }
+
+export function UploadResourceV2(props) {
+  const { file, cid, defaultFileName = '', onProgress = () => {} } = props;
+  return getFileMD5(file)
+    .then((md5) => {
+      return nkcAPI('/rs/md5', 'POST', {
+        md5,
+        filename: file.name,
+      });
+    })
+    .then((data) => {
+      if (!data.uploaded) {
+        const formData = new FormData();
+        formData.append('file', file, file.name || defaultFileName);
+        if (cid) {
+          formData.append('cid', cid);
+        }
+        return nkcUploadFile('/r', 'POST', formData, onProgress);
+      } else {
+        return Promise.resolve(data);
+      }
+    });
+}
