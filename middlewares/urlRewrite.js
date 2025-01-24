bcc15f9f4dfa75a3e564c1887dd0f967d64be6f8
pxgo <pengxiguaa@gmail.com>
2025-01-24 10:11:01 +0800

    feat: 兼容已斜线结尾的url

--- middlewares/urlRewrite.js
+++ middlewares/urlRewrite.js
@@ -1,27 +1,34 @@
 const rs = [
   {
     map: /^\/read\.php\?.*tid=([0-9]{1,10})/,
-    to:'/t/$1'
+    to: '/t/$1',
   },
   {
     map: /^\/index\.php\?.*fid=([0-9]{1,10})/,
-    to:'/f/$1'
+    to: '/f/$1',
   },
   {
     map: /^\/read-kc-tid-([0-9]{1,10}).*\.html/,
-    to: '/t/$1'
-  }
+    to: '/t/$1',
+  },
+  {
+    map: /^(.+?)\/(\?.*)?$/,
+    to: '$1$2',
+  },
 ];
 
 module.exports = (ctx, next) => {
-  const {url} = ctx;
-  const res = rs.find(e => {
+  const { url } = ctx;
+  const res = rs.find((e) => {
     return url.match(e.map);
   });
-  if(res) {
-    const redirectUrl = url.replace(res.map, res.to);
+  if (res) {
+    let redirectUrl = url.replace(res.map, res.to);
+    if (redirectUrl === '') {
+      redirectUrl = '/';
+    }
     ctx.status = 301;
     return ctx.redirect(redirectUrl);
   }
-  return next()
+  return next();
 };
