36bafa93cffe25a30018777cb9a96a3a4db6accc
pxgo <stackstorm@outlook.com>
2025-12-18 17:12:30 +0800

    feat: 登录时自动注册

--- defaultData/index.js
+++ defaultData/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const apiFunction = require('../nkcModules/apiFunction');
+const { registerService } = require('../services/account/register.service');
 const fsPromises = fs.promises;
 const defaultConfigPath = path.resolve(__dirname, './config');
 const configPath = path.resolve(__dirname, `../config`);
@@ -32,7 +33,7 @@ async function initAccount() {
   }
   console.log(`creating the admin account`);
   const { username, password } = accountConfig;
-  const user = await db.UserModel.createUser({});
+  const user = await registerService.createUser({});
   await user.updateOne({
     certs: ['dev'],
     username,
---
+++ services/database/transaction.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+class TransactionService {
+  /**
+   * @param {Function} callback - 异步回调函数，接收 session 作为参数
+   * @param {ClientSession} [externalSession] - 可选的外部事务 session
+   */
+  withTransaction = async (callback, externalSession) => {
+    // 如果已经存在外部 session，直接复用并执行回调
+    if (externalSession) {
+      return await callback(externalSession);
+    }
+    // 如果没有外部 session，启动新的事务流程
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      const result = await callback(session);
+      await session.commitTransaction();
+      return result;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      session.endSession();
+    }
+  };
+}
+
+module.exports = {
+  transactionService: new TransactionService(),
+};
