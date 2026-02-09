83339d2277ec4c923e7e07c3f0695344c0cfec48
pxgo <stackstorm@outlook.com>
2026-02-09 15:56:58 +0800

    perf: 优化前台管理统计

--- pages/nkc/status/status.js
+++ pages/nkc/status/status.js
@@ -1,111 +1,145 @@
-import {detailedTime} from "../../lib/js/time";
+import { detailedTime } from '../../lib/js/time';
+import { nkcAPI } from '../../lib/js/netAPI';
+import {
+  sweetError,
+  sweetQuestion,
+  sweetSuccess,
+  screenTopWarning,
+} from '../../lib/js/sweetAlert';
 
 function initTime() {
-  $('.time').datetimepicker({
-    language:  'zh-CN',
+  window.$('.time').datetimepicker({
+    language: 'zh-CN',
     format: 'yyyy-mm',
     autoclose: 1,
     todayHighlight: 1,
     startView: 4,
     minView: 3,
-    forceParse: 0
+    forceParse: 0,
   });
 }
 
-$(function() {
+window.$(function () {
   initTime();
-  getResults({type: 'today'});
-  var radios = $('input:radio[name="statusType"]');
-  radios.on('ifChanged', function() {
-    for(var i = 0; i < radios.length; i ++) {
+  getResults({ type: 'today' });
+  var radios = window.$('input:radio[name="statusType"]');
+  radios.on('ifChanged', function () {
+    for (var i = 0; i < radios.length; i++) {
       var radio = radios.eq(i);
-      if(radio.prop('checked')) {
+      if (radio.prop('checked')) {
         var type = radio.attr('data-type');
-        getResults({type: type});
+        getResults({ type: type });
       }
     }
   });
 });
 
-/*$('input[name="statusType"]').iCheck({
+/*window.$('input[name="statusType"]').iCheck({
   checkboxClass: 'icheckbox_minimal-red',
   radioClass: 'iradio_minimal-red',
 });*/
 
-
 function getResults(options) {
   var type = options.type;
-  if(type === 'custom') {
-    $('#custom').show();
+  if (type === 'custom') {
+    window.$('#custom').show();
   } else {
-    $('#custom').hide();
-    getData(type)
+    window.$('#custom').hide();
+    getData(type);
   }
-
 }
 
 function reset() {
-  $('#custom input[name="time1"]').val('');
-  $('#custom input[name="time2"]').val('');
+  window.$('#custom input[name="time1"]').val('');
+  window.$('#custom input[name="time2"]').val('');
 }
 
 function getData(type) {
-  var url = '/nkc?type='+type;
-  if(type === 'custom') {
-    var time1 = $('#custom input[name="time1"]').val();
-    var time2 = $('#custom input[name="time2"]').val();
-    url = '/nkc?type='+type+'&time1='+time1+'&time2='+time2;
+  var url = '/nkc?type=' + type;
+  if (type === 'custom') {
+    var time1 = window.$('#custom input[name="time1"]').val();
+    var time2 = window.$('#custom input[name="time2"]').val();
+    url = '/nkc?type=' + type + '&time1=' + time1 + '&time2=' + time2;
   }
   nkcAPI(url, 'GET', {})
-    .then(function(data) {
+    .then(function (data) {
       display(data.results);
     })
-    .catch(function(data) {
-      screenTopWarning(data.error|| data);
-    })
+    .catch(function (data) {
+      screenTopWarning(data.error || data);
+    });
 }
 
 function display(results) {
-
-
-  var myChart = echarts.init(document.getElementById('main'));
-
+  var myChart = window.echarts.init(document.getElementById('main'));
 
   // 指定图表的配置项和数据
   var option = {
     title: {
-      text: ''
+      text: '',
     },
     tooltip: {
-      trigger: 'axis'
+      trigger: 'axis',
     },
     legend: {
-      data:['发表文章', '发表回复', '用户注册']
+      data: [
+        '论坛文章',
+        '论坛回复',
+        '用户注册',
+        '电文',
+        '电文评论',
+        '专栏文章',
+        '专栏回复',
+      ],
     },
     xAxis: {
-      data: results.x
+      data: results.x,
     },
     yAxis: {},
     series: [
       {
-        name: '发表文章',
+        name: '论坛文章',
         type: 'line',
-        stack: '发表文章',
-        data: results.threadsData
+        stack: '论坛文章',
+        data: results.threadsData,
       },
       {
-        name: '发表回复',
+        name: '论坛回复',
         type: 'line',
-        stack: '发表回复',
-        data: results.postsData
+        stack: '论坛回复',
+        data: results.postsData,
       },
       {
         name: '用户注册',
         type: 'line',
         stack: '用户注册',
-        data: results.usersData
-      }
-    ]
+        data: results.usersData,
+      },
+      {
+        name: '电文',
+        type: 'line',
+        stack: '电文',
+        data: results.momentsData,
+      },
+      {
+        name: '电文评论',
+        type: 'line',
+        stack: '电文评论',
+        data: results.momentCommentsData,
+      },
+      {
+        name: '专栏文章',
+        type: 'line',
+        stack: '专栏文章',
+        data: results.articlesData,
+      },
+      {
+        name: '专栏回复',
+        type: 'line',
+        stack: '专栏回复',
+        data: results.articleCommentsData,
+      },
+    ],
   };
 
   // 使用刚指定的配置项和数据显示图表。
@@ -115,13 +149,13 @@ window.getResults = getResults;
 window.reset = reset;
 window.getData = getData;
 
-const data = NKC.methods.getDataById('data');
-window.app = new Vue({
+const data = window.NKC.methods.getDataById('data');
+window.app = new window.Vue({
   el: '#app',
   data: {
     originOperations: data.operations,
     startTime: data.startTime,
-    sortType: 'count'
+    sortType: 'count',
   },
   methods: {
     detailedTime,
@@ -129,7 +163,7 @@ window.app = new Vue({
       return sweetQuestion(`确定要清除记录？`)
         .then(() => {
           return nkcAPI('/nkc', 'POST', {
-            type: 'removeStatisticsOperation'
+            type: 'removeStatisticsOperation',
           });
         })
 
@@ -140,22 +174,22 @@ window.app = new Vue({
     },
     changeSortType(sortType) {
       this.sortType = sortType;
-    }
+    },
   },
   computed: {
     operations() {
-      const {sortType, originOperations} = this;
-      return originOperations.sort(function(v1, v2) {
+      const { sortType, originOperations } = this;
+      return originOperations.sort(function (v1, v2) {
         return v2[sortType] - v1[sortType];
       });
     },
     startTimeString() {
-      if(this.startTime > 0) {
+      if (this.startTime > 0) {
         return this.detailedTime(this.startTime);
       } else {
-        return '未知'
+        return '未知';
       }
-    }
+    },
   },
 });
 
